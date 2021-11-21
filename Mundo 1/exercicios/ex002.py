988242526ae2712870f15a080d44bb82589fa9ad
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-21 18:02:28 -0300

    Criação do ex002.py

---
+++ Mundo 1/exercicios/ex002.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro numero: '))
+s = n1+n2
+# print ('A soma entre {}'.format (n1),'e {}' .format(n2), 'vale', s)
+print('A soma entre {} e {} vale {}' .format(n1, n2, s))
