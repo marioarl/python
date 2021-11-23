c92fb91f6c0b326b0b8823b120367ed720353849
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-23 11:15:07 -0300

    Criacao do ex007.py

---
+++ Mundo 1/exercicios/ex007.py
@@ -0,0 +1,12 @@
+frase = 'Curso em Vídeo Python'
+print(frase[1::15])
+
+print(""" djaldjladjlsajdlsjdlsjdlsjdlsajdlkasjdlsajdlsk
+dlksadklasldjalsjdlsajdlasdlsjdlsjdlasjd
+adsljdlkasjdlajdlsajdlsajdljasdl
+alsdjklasjdljsaldjsaldjsaldjsaljdasldjsaljdas
+dlajkldjalsdjlas""") # colocando tres aspas duplas no comeco e no final da frase
+
+print(frase.count('o')) # o Python diferencia a letra maiuscula e minuscula
+print(frase.upper().count('O'))
+print(len(frase))
