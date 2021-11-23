a9be5a869564eb6a649159371df5636aba4c6473
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-23 11:21:04 -0300

    Criacao do desafio024.py

---
+++ Mundo 1/desafios/desafio024.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou nao com nome de "Santo"
+
+#minha resposta
+cidade = str(input('Em que cidade voce nasceu? ')).strip()
+print(cidade)
+print(cidade[:5].upper() == 'SANTO')
+
+
+#resposta do Gustavo
+#cid = str(input('Em que cidade voce nasceu?')).strip()
+#print(cid[:5].upper() == 'SANTO')
