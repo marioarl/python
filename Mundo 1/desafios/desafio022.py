be6f0d8f4bd7a02a45b96f08ecbbeb466b4b427b
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-23 11:18:25 -0300

    Criacao do desafio022.py

---
+++ Mundo 1/desafios/desafio022.py
@@ -0,0 +1,30 @@
+#Crie um programa que leia o nome completo da pessoa e mostre:
+#* O nome com todas as letras maiusculas
+#* o nome com todas as letras minusculas
+#* Quantas letras ao tem o nome (sem considerar o espaco)
+#* Quantas letras tem o primeiro nome
+
+#minha resposta
+#nome = str(input('Digite seu nome completo: '))
+#print('Analisando seu nome...')
+#print('Seu nome em maiusculo é {}'.format (nome.upper()))
+#print('Seu nome em minusculo é {}'.format(nome.lower()))
+#print('Seu nome tem ao todo', len(nome), 'letras') #nao consigo tirar os espacos em branco
+#print('Seu primeiro nome é {} e ele tem {} letras'.format(nome.split()[0], len(nome.split()[0])))
+
+#resposta 1 do Gustavo
+#nome = str(input('Digite seu nome completo: ')).strip() #strip retira os espacos do inicio e do fim
+#print('Analisando seu nome...')
+#print('Seu nome em maiusculo é {}'.format(nome.upper()))
+#print('Seu nome em minusculo é {}'.format(nome.lower()))
+#print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' ')))
+#print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+
+#resposta 2 do Gustavo
+nome = str(input('Digite seu nome completo: ')).strip() #strip retira os espacos do inicio e do fim
+print('Analisando seu nome...')
+print('Seu nome em maiusculo é {}'.format(nome.upper()))
+print('Seu nome em minusculo é {}'.format(nome.lower()))
+print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
