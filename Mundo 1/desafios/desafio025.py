5f544e883a698bed7ab5053ba603010dfa2ef52e
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-23 11:22:13 -0300

    Criacao do desafio025.py

---
+++ Mundo 1/desafios/desafio025.py
@@ -0,0 +1,9 @@
+#Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+#minha resposta
+#nome = str(input('Qual o seu nome completo? ')).strip()
+#print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+
+#resposta do Gustavo(utilizando o operador in)
+nome = str(input('Qual o seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
