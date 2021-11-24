c9e113deb4e709376948f824f0f9abf7463a751c
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-24 10:55:47 -0300

    Criacao do desafio033.py

---
+++ Mundo 1/desafios/desafio033.py
@@ -0,0 +1,39 @@
+#Faça um programa que leia tres numeros e mostre qual é o maior e qual o menor
+
+#minha resposta
+#n1 = int(input('Primeiro valor: '))
+#n2 = int(input('Segundo valor: '))
+#n3 = int(input('Terceiro valor: '))
+#if n1 < n2 and n1 < n3:
+#    menor = n1
+#if n2 < n1 and n2 < n3:
+#    menor = n2
+#if n3 < n1 and n3 < n2:
+#    menor = n3
+#print('O menor valor digitado foi {}'.format(menor))
+#if n1 > n2 and n1 > n3:
+#    maior = n1
+#if n2 > n1 and n2 > n3:
+#    maior = n2
+#if n3 > n1 and n3 > n2:
+#    maior = n3
+#print('O maior valor digitado foi {}'.format(maior))
+
+#resposta do Gustavo
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor:'))
+c = int(input('terceiro valor:'))
+#verificando quem é menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+#verificando quem é o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
