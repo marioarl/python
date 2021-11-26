999b3b11d311de18b34e14559f4508f72b291867
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-26 11:31:25 -0300

    criacao do desafio038.py

---
+++ Mundo 2/desafios/desafio038.py
@@ -0,0 +1,26 @@
+#escreva um programa que leia dois numeros inteiros  e compare-os, mostrando na tela uma
+#mensagem:
+#O primeiro valor é maior
+#o segundo valor é maior
+#nao existe valor maior, os dois sao iguais
+
+#minha resposta
+#num1 = int(input('Primeiro numero: '))
+#num2 = int(input('Segundo numero: '))
+#if num1 > num2:
+#    print('O PRIMEIRO valor é maior')
+#elif num1 < num2:
+#    print('O SEGUNDO valor é maior')
+#elif num1 == num2:
+#    print('Os dois valores são IGUAIS')
+
+#resposta do Gustavo
+
+n1 = int(input('Primeiro numero: '))
+n2 = int(input('Segundo numero: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior')
+elif n1 < n2:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores são IGUAIS')
