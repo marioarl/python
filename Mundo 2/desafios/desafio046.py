efff49336cb35e28141b51cecdedceda0f46a450
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-27 09:04:58 -0300

    Criacao do desafio046 e 047.py

---
+++ Mundo 2/desafios/desafio046.py
@@ -0,0 +1,16 @@
+#Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de
+#artificio, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+import time
+for c in range(10, -1, -1):
+    print(c)
+    time.sleep(1)
+print('BUM! BUM! POOOOW!')
+
+#resposta do Gustavo
+#from time import sleep
+#for cont in range(10, -1, -1):
+#    print(cont)
+#    sleep(0.5)
+#print('BUM! BUM! POOOOW!')
+
---
+++ Mundo 2/desafios/desafio047.py
@@ -0,0 +1,17 @@
+#Crie um programa que mostre na tela todos os numeros pares que estao no intervalo entre 1 e 50
+
+#minha resposta
+#for par in range(2, 51, 2):
+#    print(par, end=' ') #o comando end=' ' coloca as linhas horizontais em verticais
+#print('Acabou')
+
+#resposta1 do Gustavo
+#for n in range(1, 51):
+#    if n % 2 == 0:
+#        print(n, end=' ')
+#print('Acabou')
+
+#resposta2 do Gustavo
+#for n in range(2, 51, 2):
+#    print(n, end=' ')
+#print('Acabou')
