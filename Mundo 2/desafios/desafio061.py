17cd43b86b850b7820a857f5e8540c21804e3020
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-29 11:00:59 -0300

    criacao desafio061.py

---
+++ Mundo 2/desafios/desafio061.py
@@ -0,0 +1,16 @@
+#refaça o DESAFIO051, lendo o primeiro termo de uma PA, mostrando os 10 primeiros termos
+# da progressao usando a estrutura WHILE
+#sem utilizar a formula da PA
+print('='*40)
+print('{:^40}'.format('10 TERMOS DE UMA PA'))
+print('='*40)
+t1 = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+termo = t1
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
+print(cont)
