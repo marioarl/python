2c10cab119f684cabf041760f240974e6dc3d026
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-29 11:05:22 -0300

    Criacao desafio065.py

---
+++ Mundo 2/desafios/desafio065.py
@@ -0,0 +1,20 @@
+#crie um programa que leia varios numeros inteiros pelo teclado. No final da execucao, mostre
+#a media entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao
+#usuario se ele quer ou nao continuar a digitar
+resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um numero: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print('Voce digitou {} numeros e a média foi {}'.format(quant, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
