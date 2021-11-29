1c7cdb6da301db7fcb4e5799a20d10ec8c673eea
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-29 11:00:16 -0300

    Criacao desafio060.py

---
+++ Mundo 2/desafios/desafio060.py
@@ -0,0 +1,30 @@
+#faça um programa que leia um numero qualuqer e mostre seu fatorial
+#EX: 5!=5X4X3X2X1 = 120
+#print('Digite um numero para')
+#num = int(input('calcular seu fatorial: '))
+#fat2 = 0
+#for c in range(1, num, -1):
+#    fat = num * c
+#    print('Calculando {}! = {} X {}'.format(num, num, c))
+
+#resposta1 Gustavo(com modulo math
+#from math import factorial
+#n = int(input('Digite um número para calcular seu fatorail: '))
+#f = factorial(n)
+#print('O fatorial de {} é {}'.format(n, f))
+
+#resposta2 Gustavo
+n = int(input('Digite um numero para calcular seu fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x 'if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
+
+
+
