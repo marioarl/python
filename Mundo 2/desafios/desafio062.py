a30f9588ebd2e96356bbf4c8e6ff282dfb8ebc40
Mario Augusto Rego LIma <supermario_3@live.com>
2021-11-29 11:01:50 -0300

    Criacao desafio062.py

---
+++ Mundo 2/desafios/desafio062.py
@@ -0,0 +1,20 @@
+#melhore o desafio 061, perguntando para o usuario se ele quer mostrar mais alguns termos.
+#O programa encerra quando ele disser que quer mostrar 0(zero) termos.
+print('='*40)
+print('{:^40}'.format('10 TERMOS DE UMA PA'))
+print('='*40)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos voce quer mostrar a mais? '))
+print('Progressao finalizada com {} termos mostrados'.format(total))
