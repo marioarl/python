18baf40cb24d7bc5b619713e0f1afb889102450c
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-01 10:40:38 -0300

    Criacao desafio079.py

---
+++ Mundo 3/desafios/desafio079.py
@@ -0,0 +1,18 @@
+#crie um programa onde o usuario possa digitar varios valores numericos e cadastre-os em uma
+#lista. Caso o numero já exista lá dentro, ele nao será adicionado. No final serao exibidos todos
+#os valores unicos digitados em ordem crescente
+num = []
+while True:
+    n = (int(input('Digite um valor: ')))
+    if n not in num:
+        num.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if cont == 'N':
+        break
+print('-=-'*15)
+num.sort()
+print(f'Voce digitou os valores {num}')
+
