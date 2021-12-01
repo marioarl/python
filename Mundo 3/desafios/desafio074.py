b711c7e5c8eee49092802f89439414d3843877a8
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-01 10:27:01 -0300

    Criacao desafio074.py

---
+++ Mundo 3/desafios/desafio074.py
@@ -0,0 +1,23 @@
+#Crie um programa que vai gerar cinco numeros aleatorios e colocar em uma TUPLA.
+#Depois disso, mostre a listagem de numeros gerados e tambem indique o menor valor e o maior valor que
+#estao na TUPLA
+
+#minha resposta
+from random import randint
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print('Os valores sorteados foram:', end=' ')
+for n in numeros:
+    print(n, end= ' ')
+print(f'\nO maior valor sorteado foi {max(numeros)}')
+print(f'O menor valor sorteado foi {min(numeros)}')
+
+#resposta do Gustavo
+
+#from random import randint
+#numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+#print('Os valores sorteados foram:', end=' ')
+#for n in numeros:
+#    print(f'{n}', end=' ')
+#print(f'\nO maior valor sorteado foi {max(numeros)}')
+#print(f'O menor valor sorteado foi {min(numeros)}')
+
