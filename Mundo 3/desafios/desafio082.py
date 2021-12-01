5e71bd4719c5e00229476d2244284fa83de0e360
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-01 10:42:41 -0300

    Criacao desafio082.py

---
+++ Mundo 3/desafios/desafio082.py
@@ -0,0 +1,49 @@
+#crie um programa que leia varios numeros e coloque em uma lista.
+#Depois disso crie 2 listas extras que vao conter apenas os valores pares e eu outra lista os valores impares
+#Ao final mostre o conteudo das 3 listas geradas
+
+#minha resposta
+lista = []
+par = []
+impar = []
+while True:
+    lista.append(int(input('Digite um numero: ')))
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('-='*20)
+print(f'A lista completa é {lista}')
+for c, v in enumerate(lista):
+    if v % 2 == 0:
+        par.append(v)
+    else:
+        impar.append(v)
+print(f'A lista de pares é {par}')
+print(f'A lista de impares é {impar}')
+
+
+#resposta do Gustavo
+num = list()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um numero: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        impares.append(v)
+print('-='*30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
+
+
+
+
+
