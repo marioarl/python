33f740e5e043f02808e462f896c0842eef26ad81
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-01 10:23:46 -0300

    Criacao do ex016.py

---
+++ Mundo 3/exercicios/ex016.py
@@ -0,0 +1,55 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+#print(lanche[1:3]) # só irá imprimir Suco e Pizza
+
+#lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+#print('-'*30)
+#
+#for cont in range (0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posicao {cont}')
+#print('-'*30)
+#
+#for pos, comida in enumerate (lanche):
+#    print(f'Eu vou comer {comida} na posicao {pos}')
+#print('-'*30)
+#print('Comi pra caramba!!!!')
+
+#lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+#print(sorted(lanche)) # Para imprimir em ordem
+
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b # Concatenar as TUPLAS
+#print(c)
+#
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b # Concatenar as TUPLAS
+#print(len(c))
+#
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b # Concatenar as TUPLAS
+#print(c.count(5)) #contar quantos numeros 5 tem nas TUPLAS
+#
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b # Concatenar as TUPLAS
+#print(c)
+#print(c.index(8)) #em que posicao está o numero 8
+#
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b # Concatenar as TUPLAS
+#print(c)
+#print(c.index(2)) #em que posicao está o numero 2, como existem 2 x o numero 2, ele pega a primeira ocorrencia
+
+#pessoa = ('Gustavo', 39, 'M', 99.88) #pode ter tipos diferentes dentro de uma TUPLA
+#print(pessoa)
+
+
+pessoa = ('Gustavo', 39, 'M', 99.88) #pode ter tipos diferentes dentro de uma TUPLA
+del (pessoa) # apagar a TUPLA
+print(pessoa)
