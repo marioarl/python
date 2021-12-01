047f63e46b10d913c39c5dbe70dddba783fb6d75
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-01 10:28:45 -0300

    Criacao desafio076.py

---
+++ Mundo 3/desafios/desafio076.py
@@ -0,0 +1,14 @@
+#crie um programa que tenha uma unica TUPLA com nomes de produtos e seus respectivos precos
+# na sequencia.
+#No final mostre uma listagem de precos organizando os dados em forma tabular
+print('-'*40)
+print(f'{"LISTAGEM DE PRECOS":^40}')
+print('-'*40)
+listagem = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20,
+            'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
+for l in range(0, len(listagem)):
+    if l % 2 == 0:
+        print(f'{listagem[l]:.<30}', end='')
+    else:
+        print(f'R${listagem[l]:>7.2f}')
+print('-'*40)
