c7da5400b93845c22db25782ac4a17ca73457918
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-01 10:29:34 -0300

    Criacao desafio077.py

---
+++ Mundo 3/desafios/desafio077.py
@@ -0,0 +1,10 @@
+#crie um programa que tenha uma TUPLA com varias palavras (nao usar acento). Depois disso, voce
+#deve mostrar, para cada palavra, quais sao as suas vogais
+
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar',
+            'trabalhar', 'mercado', 'programador', 'futuro')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
