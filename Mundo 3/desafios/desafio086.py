0dc642d819f2de0b2798bf88a644f039e8f80b23
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-02 09:51:02 -0300

    Criacao desafio086.py

---
+++ Mundo 3/desafios/desafio086.py
@@ -0,0 +1,36 @@
+#crie um programa que crie uma matriz de dimensao 3x3 e preencha com valores lidos pelo teclado.
+#   ___________________    No final mostre a matriz na tela, com a formatacao correta
+#  0|     |     |     |
+#   |_____|_____|_____|
+#  1|     |     |     |
+#   |_____|_____|_____|
+#  2|     |     |     |
+#   |_____|_____|_____|
+#      0     1      2
+
+#minha resposta
+num = [[], [], []]
+for n in range(0, 3):
+    valor = int(input(f'Digite um valor para [0, {n}]: '))
+    num[0].append(valor)
+for n in range(0, 3):
+    valor = int(input(f'Digite um valor para [1, {n}]: '))
+    num[1].append(valor)
+for n in range(0,3):
+    valor = int(input(f'Digite um valor para [2, {n}]: '))
+    num[2].append(valor)
+print('-='*30)
+print(f'[  {num[0][0]:^5}  ]', f'[  {num[0][1]:^5}  ]', f'[  {num[0][2]:^5}  ]')
+print(f'[  {num[1][0]:^5}  ]', f'[  {num[1][1]:^5}  ]', f'[  {num[1][2]:^5}  ]')
+print(f'[  {num[2][0]:^5}  ]', f'[  {num[2][1]:^5}  ]', f'[  {num[2][2]:^5}  ]')
+
+#resposta Gustavo
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-='*30)
+for l in range(0,3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
