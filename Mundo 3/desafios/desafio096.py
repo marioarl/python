d15a4179fa45ce8f8e2c1080d361204ac02870ae
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-04 09:12:42 -0300

    Criacao desafio096.py

---
+++ Mundo 3/desafios/desafio096.py
@@ -0,0 +1,29 @@
+#crie um programa que tenha uma funcao chamada area(), que receba as dimensoes de um terreno retangular
+# (largura e comprimento) e mostre a area do terreno
+
+#minha resposta
+#def area(larg, c):
+#    a = larg * c
+#    print(f'A area de um terreno {larg}x{c} é de {a}m2.')
+
+#Programa Principal
+#print('Controle de Terernos')
+#print('-'*20)
+#larg = float(input('LARGURA (m): '))
+#c = float(input('COMPRIMENTO (m): '))
+#area(larg, c)
+
+
+#resposta do Gustavo
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg}x{comp} é de {a}m2.')
+
+
+#Programa principal
+print('Controle de Terrenos')
+print('-' * 20)
+l= float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m) '))
+area(l, c)
+
