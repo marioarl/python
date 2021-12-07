0be080d490c7c6b5d1dee2576e4b9b54f9cc23f6
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-07 11:33:16 -0300

    Criacao do pacote moeda.py

---
+++ Mundo 3/ex107/moeda.py
@@ -0,0 +1,19 @@
+def metade(n):
+    res = n / 2
+    return res
+
+
+def dobro(n):
+    res = n * 2
+    return res
+
+
+def aumentar(n, perc=0):
+    res = n + (n * (perc/100))
+    return res
+
+
+def diminuir(n, perc=0):
+    res = n - (n * (perc/100))
+    return res
+
