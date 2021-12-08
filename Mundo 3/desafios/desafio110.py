39c3f452e131eedb29102820f39df93f1a4b2378
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-08 07:45:22 -0300

    Criacao desafio110.py

---
+++ Mundo 3/desafios/desafio110.py
@@ -0,0 +1,6 @@
+#Adicione ao modulo moeda.py criado nos desafios anteriores, uma funcao chamada resumo(), que mostra na
+#tela algumas informacoes geradas pelas funcoes que já temos no modulo criado até aqui
+from ex110 import moeda
+
+p = float(input('Digite o preço: R$'))
+moeda.resumo(p, 15, 30)
