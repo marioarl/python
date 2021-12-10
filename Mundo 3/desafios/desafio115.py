b6d47e939c451249f70241ace2fd359525843626
Mario Augusto Rego LIma <supermario_3@live.com>
2021-12-10 10:24:42 -0300

    Criacao desafio115.py

---
+++ Mundo 3/desafios/desafio115.py
@@ -0,0 +1,46 @@
+#Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo
+# de texto simples. O sistema só vai ter 2 opcoes: cadastrar uma nova pessoa e listar todas as pessoas
+#cadastradas
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mErro: por favor, digite um numero inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuario preferiu nao digitar esse numero.\033[m')
+            return 0
+        else:
+            return n
+
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resposta == 1:
+        #Opcao de listar o conteudo de um arquivo
+        lerArquivo(arq)
+    elif resposta == 2:
+        #Opcao de cadastrar uma pessoa
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... Até logo')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opcao válida!\033[m')
+    sleep(1.5)
+
+
+
