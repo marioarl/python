507940c209221d4984abff74f8c16236216f8f28
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-28 17:22:59 -0300

    Alterado nome de arquivo

--- Mundo 1/desafios/desafio032-Ano Bissexto.py
+++ Mundo 1/desafios/desafio032-Ano Bissexto.py
@@ -15,7 +15,7 @@
 from datetime import date
 ano = int(input('Que ano quer analisar? Coloque 0 para o ano atual: '))
 if ano == 0:
-    ano = date.today().year #esta instrucao vai pegar o ano atula config na maquina
+    ano = date.today().year #esta instrucao vai pegar o ano atual configurado na maquina
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é BISSEXTO'.format(ano))
 else:
