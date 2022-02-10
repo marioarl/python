2b2935b7664a3c2702760613b29b0e9ff08a4614
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-09 22:07:14 -0300

    Update ex010.py

--- Mundo 1/exercicios/ex010.py
+++ Mundo 1/exercicios/ex010.py
@@ -1,3 +1,15 @@
+# Cores no terminal
+# Exemplo: \033[0;33;44m onde:
+# \033[ ==> ANSI escape sequence
+# 0 ==> ESTILO
+# 33 ==> TEXTO
+# 44 ==> Background
+
+# ESTILO = 0 - None , 1 - Bold, 4 - Underline, 7 - Negativo
+# TEXTO  = 30 - Branco, 31 - Vermelho, 32 - Verde, 33 - Amarelo, 34 - Azul, 35 - magenta, 36 - Ciano, 37 - Cinza
+# BG     = 40 - Branco, 41 - Vermelho, 42 - Verde, 43 - Amarelo, 44 - Azul, 45 - Magenta, 46 - Ciano, 47 - Cinza             
+# Para delimitar onde termina o escape sequence, devemos colocar \033[m
+
 print('\033[4;30;45mOlá Mundo!\033[m')
 
 a = 3
