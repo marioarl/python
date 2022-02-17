6804c2f860de6d5569af248b5fee372808ea001e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-17 09:55:41 -0300

    Exclusao de arquivo

--- Mundo 1/Aulas/ex006.py
+++
@@ -1,8 +0,0 @@
-# ver quais as bibliotecas que posso importar? acessar o python.org >docs>library reference
-# ESTUDAR
-
-#clicar em PyPi (Python Packet Index)
-
-#importando a biblioteca emoji e imprimrindo na tela
-import emoji
-print(emoji.emojize('Nada :no_mouth:', use_aliases=True))
