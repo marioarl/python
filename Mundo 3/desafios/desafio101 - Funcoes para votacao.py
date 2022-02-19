7b69def82a61c4486b5529b3603112228cba4588
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-19 12:25:33 -0300

    Alterado nome de arquivo

---
+++ Mundo 3/desafios/desafio101 - Funcoes para votacao.py
@@ -0,0 +1,20 @@
+#Crie um programa que tenha uma funcao chamada voto() que vai receber como parametro o ano de nascimento
+#de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas
+#eleicoes.
+
+#minha resposta
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA'
+    if 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATORIO'
+
+
+#Programa principal
+nasc = int(input('Em que ano voce nasceu? '))
+print(voto(nasc))
