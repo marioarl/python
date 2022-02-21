9c4589e8b9a64acecf3185927c4bf662344a9ecd
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-21 08:21:40 -0300

    Delete desafio109.py

--- Mundo 3/desafios/desafio109.py
+++
@@ -1,9 +0,0 @@
-#Modifique as funcoes que foram criadas no desafio107 para que elas aceitem um parametro a mais,
-#informando se o valor retornado por elas vai ser ou nao formatado pela funcao moeda(), desenvolvida no
-#desafio108
-from ex109 import moeda
-p = float(input('Digite o preço: R$ '))
-print(f'A metade de {moeda.moeda(p)} é {(moeda.metade(p, True))}')
-print(f'O dobro de {moeda.moeda(p)} é {(moeda.dobro(p, True))}')
-print(f'Aumentando 10%, temos {(moeda.aumentar(p, 10, True))}')
-print(f'Reduzindo 13%, temos {(moeda.diminuir(p, 13, True))}')
