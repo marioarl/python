7ecfd46d3929308a567e7bfc9b9e9b57aa5834cd
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-22 12:09:37 -0300

    Delete desafio112.py

--- Mundo 3/desafios/desafio112.py
+++
@@ -1,8 +0,0 @@
-#Dentro do pacote utilidadesCev que criamos no desafo111, temos o modulo chamado dado. Crie uma funcao
-# chamada leiaDinheiro() que seja capaz de funcionar com a funcao input(), mas com uma validacao de dados
-#para aceitar apenas valores que sejam monetarios.
-from ex112.utilidadescev import moeda
-from ex112.utilidadescev import dado
-
-p = dado.leiaDinheiro('Digite o preço R$')
-moeda.resumo(p, 25, 30)
