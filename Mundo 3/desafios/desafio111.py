0d72391c991bba276e5ff007c0093a58204e80e5
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-22 12:09:13 -0300

    Delete desafio111.py

--- Mundo 3/desafios/desafio111.py
+++
@@ -1,6 +0,0 @@
-#Crie um pacote chamado utilidadesCev que tenha dois modulos internos chamados moeda e dado.
-#Transfira todos as funcoes utilizadas nos desafios 107 até 110 para o primeiro pacote e mantenha tudo funcionando.
-from ex111.utilidadescev import moeda
-
-p = float(input('Digite o preço: R$'))
-moeda.resumo(p)
