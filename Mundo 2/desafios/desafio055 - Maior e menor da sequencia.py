bb80f1eed22d3f56c4d692168939260ee9f75492
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-25 10:04:56 -0300

    Update desafio055 - Maior e menor da sequencia.py

--- Mundo 2/desafios/desafio055 - Maior e menor da sequencia.py
+++ Mundo 2/desafios/desafio055 - Maior e menor da sequencia.py
@@ -1,7 +1,20 @@
 # faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor
 # peso lidos
 
-#minha resposta(nao consegui)
+#minha resposta
+maior = menor = 0
+for p in range(1,6):
+    peso = float(input(f"Peso da {p}a. pessoa: "))
+    if p == 1:
+        maior = menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+
+print(f"O maior peso lido foi de {maior:.1f}Kg")
+print(f"O menor peso lido foi de {menor:.1f}Kg")
 
 #resposta Gustavo
 maior = 0
