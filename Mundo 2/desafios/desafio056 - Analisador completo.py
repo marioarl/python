8a485ca88dbc25b6b2f1cb3fd8d607e5c95a75f1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-25 10:19:03 -0300

    Update desafio056 - Analisador completo.py

--- Mundo 2/desafios/desafio056 - Analisador completo.py
+++ Mundo 2/desafios/desafio056 - Analisador completo.py
@@ -2,6 +2,30 @@
 # A media de idade do grupo
 # Qual é o nome do homem mais velho
 # Quantas mulheres tem menos de 21 anos
+
+"""minha resposta
+somaIdade = homemMaisVelho = mulherMenos20 = 0
+nomeMaisVelho = ""
+for p in range(1,5):
+    print(f"---- {p}a. PESSOA ----")
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip().upper()
+    somaIdade += idade
+    if sexo in "M" and p ==1:
+        homemMaisVelho = idade
+        nomeMaisVelho = nome
+    else:
+        if idade > homemMaisVelho:
+            homemMaisVelho = idade
+            nomeMaisVelho = nome
+    if sexo in "F" and idade < 20:
+        mulherMenos20 += 1
+media = somaIdade / 4
+print(f"A média de idade do grupo é de {media} anos")
+print(f"O homem mais velho tem {homemMaisVelho} anos e se chama {nomeMaisVelho}")
+print(f"Ao todo são {mulherMenos20} mulheres com menos de 20 anos")"""
+
 somaidade = velho = mulherid = 0
 nvelho = ' '
 for c in range(1, 5):
