50472ca1f18141eed36c9d42aa93fd6de7fec80d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-25 09:57:37 -0300

    Update desafio054 Grupo de maioridade.py

--- Mundo 2/desafios/desafio054 Grupo de maioridade.py
+++ Mundo 2/desafios/desafio054 Grupo de maioridade.py
@@ -1,6 +1,17 @@
 #crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas
 #ainda nao atingiram a maioridade e quantas ja sao maiores.
-#minha resposta (nao consegui)
+#minha resposta
+from datetime import date
+maior = menor = 0
+for a in range(1,8):
+    nasc = int(input(f"Em que ano a {a}a. pessoa nasceu? "))
+    idade = date.today().year - nasc
+    if idade >=21:
+        maior += 1
+    else:
+        menor += 1
+print(f"Ao todo tivemos {maior} pessoas maiores de idade")
+print(f"E tambem tivemos {menor} pessoas menores de idade")
 
 
 #resposta Gustavo
