6c4254566cd6dca8e46ef92b530be69b0e25aead
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-25 10:26:40 -0300

    Update desafio056 - Analisador completo.py

--- Mundo 2/desafios/desafio056 - Analisador completo.py
+++ Mundo 2/desafios/desafio056 - Analisador completo.py
@@ -26,25 +26,27 @@ print(f"A média de idade do grupo é de {media} anos")
 print(f"O homem mais velho tem {homemMaisVelho} anos e se chama {nomeMaisVelho}")
 print(f"Ao todo são {mulherMenos20} mulheres com menos de 20 anos")"""
 
-somaidade = velho = mulherid = 0
-nvelho = ' '
-for c in range(1, 5):
-    print('-'*6, '{}a. PESSOA'.format(c), '-'*6)
-    nome = str(input('Nome: ')).strip()
-    idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).strip().upper()
+#resposta Gustavo
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ""
+totmulher20 = 0
+for p in range(1,5):
+    print("----- {}a. PESSOA -----".format(p))
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
     somaidade += idade
-    if c == 1:
-        velho = idade
-        nvelho = nome
-    else:
-        if idade > velho:
-            velho = idade
-            nvelho = nome
-    if sexo == 'F':
-        if idade < 20:
-            mulherid += 1
-
-print('A média de idade do grupo é de {:.1f} anos'.format(somaidade / 4))
-print('O homem mais velho tem {} anos e se chama {}'.format(velho, nvelho))
-print('Ao todo são {} mulheres com menos de 20 anos'.format(mulherid))
+    if p == 1 and sexo in "Mm":
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in "Mm" and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in "Ff" and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print("A media de idade do grupo é de {} anos".format(mediaidade))
+print("O homem mais velho tem {} anos e se chama {}".format(maioridadehomem, nomevelho))
+print("Ao todo são {} mulheres com menos de 20 anos".format(totmulher20))
