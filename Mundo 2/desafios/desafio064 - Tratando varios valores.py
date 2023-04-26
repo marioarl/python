236a21825193ef2d8ac01e013877009c3e8606a4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-26 07:47:03 -0300

    Update desafio064 - Tratando varios valores.py

--- Mundo 2/desafios/desafio064 - Tratando varios valores.py
+++ Mundo 2/desafios/desafio064 - Tratando varios valores.py
@@ -1,6 +1,20 @@
 #crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar quando o
 #usuario digitar o valor 999, que é a condicao de parada. No final mostre quantos numeros foram digitados
 #e qual foi a soma entre eles, desconsiderando o 999.
+
+""""
+Minha resposta
+cont = soma = 0
+num = int(input("Digite um numero [999 para parar]: "))
+while num != 999:
+    cont+=1
+    soma += num
+    num = int(input("Digite um numero [999 para parar]: "))
+print(f"Voce digitou {cont} numeros e a soma entre eles foi {soma}")
+
+"""
+
+#Resposta
 cont = soma = 0
 numero = int(input('Digite um numero [999 para parar]: '))
 while numero != 999:
