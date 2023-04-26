781b32cf8f3d2a53ef0bfdc8220d6d4f1d4b86b7
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-26 20:50:44 -0300

    Update desafio068 - Jogo par ou impar.py

--- Mundo 2/desafios/desafio068 - Jogo par ou impar.py
+++ Mundo 2/desafios/desafio068 - Jogo par ou impar.py
@@ -2,6 +2,45 @@
 #jogador PERDER, mostrando o total de vitorias consecutivas que ele conquistou no final do jogo
 from random import randint
 
+#minha resposta
+""""
+from random import randint
+v = 0
+print("-="*20)
+print("VAMOS JOGAR PAR OU IMPAR".center(40))
+print("-="*20)
+
+while True:
+    comp = randint(0,10)
+    jog = int(input("Diga um valor: "))
+    opc = str(input("Par ou Impar? [P/I] ")).strip().upper()
+    print("-"*40)
+    total = comp + jog
+    print(f"Voce jogou {jog} e o computador {comp}. Total de {total} ", end="")
+    if opc == "P":
+        if total % 2 == 0:
+            print("DEU PAR")
+            print("-"*40)
+            print("Voce VENCEU!")
+            v += 1
+        else:
+            print("DEU IMPAR")
+            print("-"*40)
+            print("Voce PERDEU")
+            break
+    if opc == "I":
+        if total %2 == 1:
+            print("DEU IMPAR")
+            print("-"*40)
+            print("Voce VENCEU! ")
+            v += 1
+        else:
+            print("DEU PAR")
+            print("Voce PERDEU")
+            break
+print(f"GAME OVER! Voce venceu {v} vezes.")
+"""
+
 #resposta do Gustavo
 v = 0
 print('=-='*15)
