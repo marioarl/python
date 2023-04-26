c7a7e3fa887311a486073488cb24faa4efe1fe6c
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-26 17:31:38 -0300

    Update desafio065 - Maior e menor valores.py

--- Mundo 2/desafios/desafio065 - Maior e menor valores.py
+++ Mundo 2/desafios/desafio065 - Maior e menor valores.py
@@ -2,6 +2,28 @@
 #a media entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao
 #usuario se ele quer ou nao continuar a digitar
 
+#minha resposta
+""""
+cont = soma = maior = menor = 0
+opc = "S"
+while opc in "S":
+    num = int(input("Digite um numero: "))
+    cont += 1
+    soma += num
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    opc = str(input("Continua? [S/N] ")).strip().upper
+media = soma / cont
+print(f"Voce digitou {cont} numeros e a média foi {media}")
+print(f"O maior valor foi {maior} e o menor foi {menor}")
+"""
+
+
 #resposta do Gustavo
 resp = 'S'
 soma = quant = media = maior = menor = 0
