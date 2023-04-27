013f9e51b1fd92d9b6c3cf696ae5f5f4e3a9cd07
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-27 10:13:31 -0300

    Update desafio070 - Estatisticas em produtos.py

--- Mundo 2/desafios/desafio070 - Estatisticas em produtos.py
+++ Mundo 2/desafios/desafio070 - Estatisticas em produtos.py
@@ -54,4 +54,22 @@ for c in listam:
     print(f'{c}, ', end='')
 print(f'\nO produto mais barato foi \033[33m{barato}\033[m e custa R${menor:.2f}')"""
 #resposta do Gstavo
-
+total = totmil = menor = cont = 0
+barato = ""
+while True:
+    produto = str(input('Nome do Produto: '))
+    preço = float(input('Preço: R$'))
+    cont += 1
+    total += preço
+    if preço > 1000:
+        totmil += 1
+    if cont == 1 or preço < menor:
+        menor = preço
+        barato = produto
+    resp = ''
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'O total da compra foi R${total:.2f}')
