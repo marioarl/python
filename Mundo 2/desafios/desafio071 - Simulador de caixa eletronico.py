109dccd851e4c851f8db3ffb27dc35d64935a9a1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-27 10:54:09 -0300

    Update desafio071 - Simulador de caixa eletronico.py

--- Mundo 2/desafios/desafio071 - Simulador de caixa eletronico.py
+++ Mundo 2/desafios/desafio071 - Simulador de caixa eletronico.py
@@ -2,6 +2,38 @@
 #ao usuario qual será o valor a ser sacado(numero inteiro) e o programa vai informar quantas
 #cedulas de cada valor serao entregues.
 #OBS: Considere que o caixa possui cedulas de R$50 R$20 R$10 e R$1
+
+#minha resposta
+"""print("="*40)
+print("BANCO ASPA".center(40))
+print("="*40)
+valor = float(input("Qual o valor voce quer sacar? R$"))
+
+total = valor
+cedulas = 50
+total_cedulas = 0
+
+while True:
+    if total >= cedulas:
+        total -= cedulas
+        total_cedulas += 1
+    else:
+        if total_cedulas > 0:
+            print(f'Contando {total_cedulas} cedulas de R${cedulas}...')
+        if cedulas == 50:
+            cedulas = 20
+        elif cedulas == 20:
+            cedulas = 10
+        elif cedulas == 10:
+            cedulas = 1
+        total_cedulas = 0
+
+        if total == 0:
+            break
+print('='*40)
+print('FIM DA OPERAÇÃO')"""    
+
+
 #resposta do Gustavo
 print('='*30)
 print('{:^30}'.format('BANCO CEV'))
