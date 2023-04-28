0f95c8047db51d3a1a9fa707619132c8c4964309
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-28 10:22:58 -0300

    Update desafio076 - Lista de preco com Tupla.py

--- Mundo 3/desafios/desafio076 - Lista de preco com Tupla.py
+++ Mundo 3/desafios/desafio076 - Lista de preco com Tupla.py
@@ -10,7 +10,8 @@ listagem = ('Lápis', 1.75,
             'Estojo', 25.00,
             'Transferidor', 4.20,
             'Compasso', 9.99,
-            'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
+            'Mochila', 120.32,
+            'Canetas', 22.30, 'Livro', 34.90)
 for l in range(0, len(listagem)):
     if l % 2 == 0:
         print(f'{listagem[l]:.<30}', end='')
