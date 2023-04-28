80c16eca06912cf01f161ac2c7dbbfeb31a64960
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-28 10:24:49 -0300

    Update desafio076 - Lista de preco com Tupla.py

--- Mundo 3/desafios/desafio076 - Lista de preco com Tupla.py
+++ Mundo 3/desafios/desafio076 - Lista de preco com Tupla.py
@@ -1,10 +1,11 @@
 #crie um programa que tenha uma unica TUPLA com nomes de produtos e seus respectivos precos
 # na sequencia.
 #No final mostre uma listagem de precos organizando os dados em forma tabular
+#minha resposta
 print('-'*40)
 print(f'{"LISTAGEM DE PRECOS":^40}')
 print('-'*40)
-listagem = ('Lápis', 1.75,
+lista = ('Lápis', 1.75,
             'Borracha', 2.00,
             'Caderno', 15.90,
             'Estojo', 25.00,
@@ -13,9 +14,11 @@ listagem = ('Lápis', 1.75,
             'Mochila', 120.32,
             'Canetas', 22.30,
             'Livro', 34.90)
-for l in range(0, len(listagem)):
+for l in range(0, len(lista)):
     if l % 2 == 0:
-        print(f'{listagem[l]:.<30}', end='')
+        print(f'{lista[l]:.<30}', end='')
     else:
-        print(f'R${listagem[l]:>7.2f}')
+        print(f'R${lista[l]:>7.2f}')
 print('-'*40)
+
+#resposta do Gustavo
