0544d05e1e17f387f905a7c49ab72f2b4ea841b8
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-28 10:20:00 -0300

    Update desafio075 - Analise de dados em Tupla.py

--- Mundo 3/desafios/desafio075 - Analise de dados em Tupla.py
+++ Mundo 3/desafios/desafio075 - Analise de dados em Tupla.py
@@ -2,26 +2,25 @@
 #A)Quantas vezes apareceu o valor 9
 #B)Em que posicao foi digitado o primeiro valor 3
 #C)Quais foram os numeros pares
-#minha resposta - nao consegui fazer os numeros pares
-ns = (int(input('Digite um numero: ')),
-           int(input('Digite outro numero: ')),
-           int(input('Digite mais um numero: ')),
-           int(input('Digite o ultimo numero: ')))
-cont = 0
-print('Voce digitou os valores', ns)
-if ns[0] == 9:
-    cont += 1
-if ns[1] == 9:
-    cont += 1
-if ns[2] == 9:
-    cont += 1
-if ns[3] == 9:
-    cont += 1
-print(f'O valor 9 apareceu {cont} vezes')
-if ns[0] != 3 and ns[1] != 3 and ns[2] != 3 and ns[3] != 3:
-    print('O valor 3 nao foi digitado em nemhuma posicao')
+#minha resposta 
+from os import system
+system('cls')
+num = (int(input('Digite um numero: ')), 
+       int(input('Digite outro numero: ')),
+       int(input('Digite mais um numero: ')),
+       int(input('Digite o ultimo numero: ')))
+
+print(f'Voce digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)}a. posição')
 else:
-    print(f'O valor 3 apareceu na {ns.index(3) + 1}a. posicao')
+    print(f'O valor 3 não foi digitado')
+
+print('Os valores pares digitados foram ', end="")
+for p in num:
+    if p % 2 == 0:
+        print(f'[{p}]', end="")
 
 #resposta do Gustavo
 num = (int(input('Digite um numero: ')),
