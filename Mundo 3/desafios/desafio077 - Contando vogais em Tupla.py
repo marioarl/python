44d55f870e464411d25266989eda6c5d3af00722
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-04-28 10:31:09 -0300

    Update desafio077 - Contando vogais em Tupla.py

--- Mundo 3/desafios/desafio077 - Contando vogais em Tupla.py
+++ Mundo 3/desafios/desafio077 - Contando vogais em Tupla.py
@@ -1,7 +1,8 @@
 #crie um programa que tenha uma TUPLA com varias palavras (nao usar acento). Depois disso, voce
 #deve mostrar, para cada palavra, quais sao as suas vogais
 
-palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar',
+palavras = ('aprender', 'programar', 'linguagem', 'python', 
+            'curso', 'gratis', 'estudar', 'praticar',
             'trabalhar', 'mercado', 'programador', 'futuro')
 for p in palavras:
     print(f'\nNa palavra {p.upper()} temos ', end='')
