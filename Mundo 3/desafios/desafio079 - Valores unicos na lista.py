7ec32a79065960fbb8ec1ceaedaa06e90f121f75
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-02 11:56:26 -0300

    Update desafio079 - Valores unicos na lista.py

--- Mundo 3/desafios/desafio079 - Valores unicos na lista.py
+++ Mundo 3/desafios/desafio079 - Valores unicos na lista.py
@@ -27,3 +27,8 @@ while True:
     else:
         print('Valor duplicado! Não vou adicionar...')
     r = str(input('Quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+print('-=' * 30)
+numeros.sort()
+print(f'Voce digitou os valores {numeros}')
