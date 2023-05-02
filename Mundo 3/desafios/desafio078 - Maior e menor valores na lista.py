c0961ecb0111b1804feca01a105e6c4fbd30acfc
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-02 11:36:53 -0300

    Update desafio078 - Maior e menor valores na lista.py

--- Mundo 3/desafios/desafio078 - Maior e menor valores na lista.py
+++ Mundo 3/desafios/desafio078 - Maior e menor valores na lista.py
@@ -61,3 +61,9 @@ print(f'O maior valor digitado foi {mai} nas posições ', end='')
 for i,v in enumerate(listanum):
     if v == mai:
         print(f'{i}... ', end="")
+print()
+print(f'O menor valor digitado foi {men} nas posições ', end="")
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}... ', end="")
+print()    
