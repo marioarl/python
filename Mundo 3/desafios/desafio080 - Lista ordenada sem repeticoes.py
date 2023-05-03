e40c1bec9d2f7ed720f5ca7b058ddd02ecfc23aa
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-03 09:40:28 -0300

    Update desafio080 - Lista ordenada sem repeticoes.py

--- Mundo 3/desafios/desafio080 - Lista ordenada sem repeticoes.py
+++ Mundo 3/desafios/desafio080 - Lista ordenada sem repeticoes.py
@@ -2,6 +2,24 @@
 #lista, já na posicao correta de insercao(sem usar o SORT())
 #No final, mostre a lista ordenada na tela
 
+#Minha resposta
+numeros = []
+for c in range(0, 5):
+    num = int(input('Digite um valor: '))
+    if c == 0 or num > numeros[-1]:
+        numeros.append(num)
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(numeros):
+            if num <= numeros[pos]:
+                numeros.insert(pos, num)
+                print(f'Adicionado na posicao {pos} da lista')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {numeros}')
+
 #resposta do Gustavo
 lista = []
 for c in range(0, 5):
