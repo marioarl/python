4fceee3c68644a181d2893d0a341f9b2156b88fb
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-03 09:46:04 -0300

    Update desafio081 - Extraindo dados de uma lista.py

--- Mundo 3/desafios/desafio081 - Extraindo dados de uma lista.py
+++ Mundo 3/desafios/desafio081 - Extraindo dados de uma lista.py
@@ -4,21 +4,21 @@ B) A lista de valores ordenada de forma decrescente
 C) Se o valor 5 foi digitado e está ou nao na lista'''
 
 #minha resposta
-#num = []
-#while True:
-#    num.append(int(input('Digite um valor: ')))
-#    cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-#    while cont not in 'SN':
-#        cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-#    if cont == 'N':
-#        break
-#num.sort(reverse=True)
-#print(f'Voce digitou {len(num)} elementos.')
-#print(f'Os valores em ordem decrescente são: {num}')
-#if 5 in num:
-#    print('O numero 5 está na lista')
-#else:
-#    print('O numero 5 nao está na lista')
+'''num = []
+while True:
+    num.append(int(input('Digite um valor: ')))
+    cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while cont not in 'SN':
+        cont = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if cont == 'N':
+        break
+num.sort(reverse=True)
+print(f'Voce digitou {len(num)} elementos.')
+print(f'Os valores em ordem decrescente são: {num}')
+if 5 in num:
+    print('O numero 5 está na lista')
+else:
+    print('O numero 5 nao está na lista')'''
 
 #resposta do Gustavo(esta resposta nao contempla se a pessoa apertar outra letra na pergunta
 #'Quer continuar? [S/N]', porque se digitar qualquer letra ele aceita como S
