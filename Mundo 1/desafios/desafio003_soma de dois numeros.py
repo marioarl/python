dc92f52e0d7473a2672d7ba1fcc7d816546430f2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-04 11:28:56 -0300

    Delete desafio003_soma de dois numeros.py

--- Mundo 1/desafios/desafio003_soma de dois numeros.py
+++
@@ -1,5 +0,0 @@
-print('\033[1;35m#\033[m'*10, '\033[1;30;46mDESAFIO 03\033[m', '\033[1;35m#\033[m'*10)
-n1 = int(input('\033[1;33mPrimeiro numero:\033[m '))
-n2 = int(input('\033[1;36mSegundo numero:\033[m '))
-r = n1 + n2
-print('A soma é', r)
