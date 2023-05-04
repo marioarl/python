162158d721be785f4e621196f017ba6b230d0627
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-04 11:23:19 -0300

    Update desafio002.py

--- Mundo 1/desafios/desafio002.py
+++ Mundo 1/desafios/desafio002.py
@@ -4,5 +4,7 @@ Crie um programa que leia dois numeros e mostre a soma entre eles
 #Minha resposta
 n1 = int(input('Digite o 1o. numero: '))
 n2 = int(input('Digite o 2o. numero: '))
+soma = n1 + n2
+print('A soma entre {} e {} = {}'.format(n1,n2,soma))
 
 
