7648404727656bd5df5045b0f57a6b02bf377380
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-04 11:26:24 -0300

    Update desafio002.py

--- Mundo 1/desafios/desafio002.py
+++ Mundo 1/desafios/desafio002.py
@@ -7,4 +7,10 @@ num2 = int(input('Digite o 2o. numero: '))
 soma = num1 + num2
 print('A soma entre {} e {} = {}'.format(num1,num2,soma))
 
+#Resposta do Gustavo
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma entre {} e {} é igual {}!.'.format(n1,n2,s))
+
 
