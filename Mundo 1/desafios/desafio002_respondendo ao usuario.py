dd5922c70f1de365fff21a58bd60ad4662e5fba0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-04 11:32:52 -0300

    Create desafio002_respondendo ao usuario.py

---
+++ Mundo 1/desafios/desafio002_respondendo ao usuario.py
@@ -0,0 +1,11 @@
+'''
+Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+'''
+
+#minha Resposta
+nome = str(input('Digite seu nome: '))
+print('É um prazer te conhecer, {}'.format(nome))
+
+#Resposta do Guanabara
+nome = str(input('Digite seu nome: '))
+print('É um prazer te conhecer, {}'.format(nome))
