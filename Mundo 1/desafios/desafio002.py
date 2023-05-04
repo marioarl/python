286c79cff7a5de7ee21876d34ea3c2a91cd4b8f9
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-04 11:22:36 -0300

    Update desafio002.py

--- Mundo 1/desafios/desafio002.py
+++ Mundo 1/desafios/desafio002.py
@@ -1,6 +1,8 @@
 ''''
 Crie um programa que leia dois numeros e mostre a soma entre eles
 '''
-
+#Minha resposta
+n1 = int(input('Digite o 1o. numero: '))
+n2 = int(input('Digite o 2o. numero: '))
 
 
