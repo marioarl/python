a87098b45401cc9fbc092119ff65257467463a8a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-04 11:19:08 -0300

    Update desafio001_ola mundo.py

--- Mundo 1/desafios/desafio001_ola mundo.py
+++ Mundo 1/desafios/desafio001_ola mundo.py
@@ -1,5 +1,5 @@
-print('======= DESAFIO 01 ======')
-nome = input('Qual é o seu nome? ')
-print('Olá {}! Prazer em te conhecer!'.format(nome))
+''''
+Crie um programa que escreva Ola Mundo! na tela
+'''
 
 
