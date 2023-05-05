c2743d86e073f1ee91cab3e8477300f92a671b8d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-05 09:38:01 -0300

    Update desafio004_dissecando a variavel.py

--- Mundo 1/desafios/desafio004_dissecando a variavel.py
+++ Mundo 1/desafios/desafio004_dissecando a variavel.py
@@ -4,13 +4,16 @@ tipo primitivo e todas as informações possiveis sobre ela
 """
 
 #minha resposta
-frase = str(input("Digite algo: ")).strip()
+frase = str(input("Digite algo: "))
 print(f"O tipo primitivo desse valor é {type(frase)}")
+print(f"Só tem espaços? ", frase.isspace())
+
+
 
 
 
 #Resposta do Gustavo
-a = input('Digite algo: ')
+'''a = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(a))
 print('Só tem espaços? ', a.isspace())
 print('É um numero? ', a.isnumeric())
@@ -18,5 +21,5 @@ print('É alfabético? ', a.isalpha())
 print('É alfanumérico? ', a.isalnum())
 print('Está em maiusculas? ', a.isupper())
 print('Está em minusculas? ', a.islower())
-print('Está capitalizada? ', a.istitle())
+print('Está capitalizada? ', a.istitle())'''
 
