cba1f280bfed06045ee8b2fb96a2b6e3a915d575
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-05 09:33:09 -0300

    Update desafio004_dissecando a variavel.py

--- Mundo 1/desafios/desafio004_dissecando a variavel.py
+++ Mundo 1/desafios/desafio004_dissecando a variavel.py
@@ -1,3 +1,9 @@
+""""
+Faça um programa que leia algo pelo teclado e mostre na tela o seu
+tipo primitivo e todas as informações possiveis sobre ela
+"""
+
+
 a = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(a))
 print('Só tem espaços? ', a.isspace())
