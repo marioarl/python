c87661206747acf55b0b60f5d6b756b25d77a339
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-06 00:26:37 -0300

    Update desafio015_aluguel de carro.py

--- Mundo 1/desafios/desafio015_aluguel de carro.py
+++ Mundo 1/desafios/desafio015_aluguel de carro.py
@@ -1,6 +1,4 @@
-#escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade
-#de dias pelos quais ele foi alugado. Calcule o preco a pagar, sabendo que o carro custa R$60,00 por dia
-#e R$0,15 por Km rodado
+'''escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preco a pagar, sabendo que o carro custa R$60,00 por dia e R$0,15 por Km rodado'''
 
 #minha resposta
 #dias = int(input('Quantos dias alugados? '))
