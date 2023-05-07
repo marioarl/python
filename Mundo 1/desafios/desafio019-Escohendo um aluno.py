78755b55fe65da0211f5a46a1f8c5ea6c3bdd06f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-07 10:45:10 -0300

    Update desafio019-Escohendo um aluno.py

--- Mundo 1/desafios/desafio019-Escohendo um aluno.py
+++ Mundo 1/desafios/desafio019-Escohendo um aluno.py
@@ -1,5 +1,7 @@
-#um professor quer sortear um dos seus 4 alunos para apagar o quadro. Faça um programa que ajude
-# ele, lendo o nome deles e escrevendo o nome escolhido
+'''
+Um professor quer sortear um dos seus 4 alunos para apagar o quadro. Faça um programa que ajude
+ele, lendo o nome deles e escrevendo o nome escolhido
+'''
 
 #minha resposta
 '''import random
