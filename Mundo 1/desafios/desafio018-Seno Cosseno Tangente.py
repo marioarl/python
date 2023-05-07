718893a3aad3b60ec14ef26aa3ff0897a6807d7c
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-07 10:41:21 -0300

    Update desafio018-Seno Cosseno Tangente.py

--- Mundo 1/desafios/desafio018-Seno Cosseno Tangente.py
+++ Mundo 1/desafios/desafio018-Seno Cosseno Tangente.py
@@ -12,3 +12,9 @@ tan = tan(radians(ang))
 print('O ângulo de {:.2f} tem o SENO de {:.2f}'.format(ang, sin))
 print('O ângulo de {:.2f} tem o COSSENO de {:.2f}'.format(ang, cos))
 print('O ângulo de {:.2f} tem a TANGENTE de {:.2f}'.format(ang, tan))
+
+#Resposta do Gustavo
+from math import radians, sin, cos, tan
+ângulo = float(input("Digite o ângulo que voce deseja: "))
+seno = sin(radians(ângulo))
+print("O ângulo de {} tem o SENO de {:.2f}".format(ângulo,seno))
