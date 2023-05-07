9f12e286a7257a7cbcfba38df2ea0036da849687
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-07 10:47:05 -0300

    Update desafio020-Sortenado uma ordem.py

--- Mundo 1/desafios/desafio020-Sortenado uma ordem.py
+++ Mundo 1/desafios/desafio020-Sortenado uma ordem.py
@@ -1,5 +1,6 @@
-# O mesmo professor do desafio anterior quer sortear a ordem de apresentaçao de trabalhos
-# dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+'''
+O mesmo professor do desafio anterior quer sortear a ordem de apresentaçao de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+'''
 
 #minha resposta
 '''import random
