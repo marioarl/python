3523540e887cc8549e08e63028e7f2bb743b1c41
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-08 11:31:35 -0300

    Update desafio023- Separando digitos.py

--- Mundo 1/desafios/desafio023- Separando digitos.py
+++ Mundo 1/desafios/desafio023- Separando digitos.py
@@ -1,9 +1,9 @@
-#Faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados.
-#Ex. Digite um numero: 1834
-# unidade: 4
-# dezena: 3
-# centena: 8
-# milhar: 1
+'''Faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados.
+Ex. Digite um numero: 1834
+ unidade: 4
+ dezena: 3
+ centena: 8
+ milhar: 1'''
 
 #minha resposta
 numero = str(input('Digite um numero inteiro: ')).strip()
