c9b23a954eeb4ac9e3706ec70298d5c0bfd39210
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-08 11:29:42 -0300

    Update desafio021-Tocando um mp3.py

--- Mundo 1/desafios/desafio021-Tocando um mp3.py
+++ Mundo 1/desafios/desafio021-Tocando um mp3.py
@@ -1,4 +1,10 @@
 '''faça um programa que abra e reproduza o audio de um arquivo mp3'''
+#Minha resposta
+import pygame
+pygame.init()
+pygame.mixer.music.load('Batida4.wav')
+pygame.mixer.music.play()
+pygame.event.wait()
 
 #resposta do Gustavo
 import pygame
