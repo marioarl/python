8b2535d1d3da79b227828672a46c48e8e2bce96b
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-09 16:39:29 -0300

    Update desafio029 -Radar eletronico.py

--- Mundo 1/desafios/desafio029 -Radar eletronico.py
+++ Mundo 1/desafios/desafio029 -Radar eletronico.py
@@ -1,5 +1,7 @@
-'''Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre
-uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite'''
+'''
+Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre
+uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite
+'''
 
 #minha resposta
 #velo = float(input('Qual é a velocidade do carro? '))
