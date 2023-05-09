1170977bb88683da44066ff35de058b3ecab9f5b
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-09 16:40:48 -0300

    Update desafio031-Custo da viagem.py

--- Mundo 1/desafios/desafio031-Custo da viagem.py
+++ Mundo 1/desafios/desafio031-Custo da viagem.py
@@ -1,6 +1,8 @@
-'''Desenvolva um programa que pergunte a distancia de uma viagem em Km.
+'''
+Desenvolva um programa que pergunte a distancia de uma viagem em Km.
 Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45
-para viagens mais longas.'''
+para viagens mais longas.
+'''
 
 #minha resposta
 #dist = float(input('Qual a distancia da sua viagem? '))
