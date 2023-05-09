7f0c1310f54c348f500d93855b9aa8f92f05fdef
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-09 16:36:59 -0300

    Update desafio027-Primerio e ultimo nome.py

--- Mundo 1/desafios/desafio027-Primerio e ultimo nome.py
+++ Mundo 1/desafios/desafio027-Primerio e ultimo nome.py
@@ -1,7 +1,7 @@
-#faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome
-# separadamente.
-#Ex. Ana Maria de Souza
-# primeiro = Ana     segundo = Souza
+'''faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome
+ separadamente.
+Ex. Ana Maria de Souza
+ primeiro = Ana     segundo = Souza'''
 
 #minha resposta
 #nome = str(input('Digite seu nome completo: ')).strip().split()
