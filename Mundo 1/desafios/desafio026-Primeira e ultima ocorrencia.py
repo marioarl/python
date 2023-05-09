cf05947551b59ddf94d85afde3acdb2d5c01c625
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-09 16:35:30 -0300

    Update desafio026-Primeira e ultima ocorrencia.py

--- Mundo 1/desafios/desafio026-Primeira e ultima ocorrencia.py
+++ Mundo 1/desafios/desafio026-Primeira e ultima ocorrencia.py
@@ -1,7 +1,7 @@
-#Faça um programa que leia uma frase pelo teclado e mostre:
-# * Quantas vezes aparece a letra "A"
-# * Em que posição ela aparece a primeira vez
-# * Em que posição ela aparece a ultima vez
+'''Faça um programa que leia uma frase pelo teclado e mostre:
+ * Quantas vezes aparece a letra "A"
+ * Em que posição ela aparece a primeira vez
+ * Em que posição ela aparece a ultima vez'''
 
 #minha resposta
 #frase = str(input('Digite uma frase: ')).strip()
