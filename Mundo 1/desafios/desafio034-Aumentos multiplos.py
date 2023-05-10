bb54078e142f4d8eaaf7690cff4b7b4d3fd0fe8e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-10 09:22:01 -0300

    Update desafio034-Aumentos multiplos.py

--- Mundo 1/desafios/desafio034-Aumentos multiplos.py
+++ Mundo 1/desafios/desafio034-Aumentos multiplos.py
@@ -1,6 +1,8 @@
-'''Escreva um programa que pergunte o salario de um funcionario e calcule o valor do seu aumento.
+'''
+Escreva um programa que pergunte o salario de um funcionario e calcule o valor do seu aumento.
 para salarios superiores a R$ 1250,00, calcule um aumento de 10%, para os inferiores ou iguais
-o aumento é de 15%'''
+o aumento é de 15%
+'''
 
 #minha resposta
 #salario = float(input('Qual é o salário do funcionario? R$ '))
