28453c0ec92477e67a469a21bebd314db05972df
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-11 14:41:40 -0300

    Update desafio039 - Alistamento militar.py

--- Mundo 2/desafios/desafio039 - Alistamento militar.py
+++ Mundo 2/desafios/desafio039 - Alistamento militar.py
@@ -1,8 +1,8 @@
-#faca um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
-# - Se ele ainda vai se alistar ao servico militar.
-# - Se é a hora de se alistar
-# - Se já passou do tempo do alistamento.
-#Seu programa tambem deverá mostrar o tempo que falta ou que passou o prazo
+'''faca um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
+ - Se ele ainda vai se alistar ao servico militar.
+ - Se é a hora de se alistar
+ - Se já passou do tempo do alistamento.
+Seu programa tambem deverá mostrar o tempo que falta ou que passou o prazo'''
 
 #minha resposta
 #from datetime import date
