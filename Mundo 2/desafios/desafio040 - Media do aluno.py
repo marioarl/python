82dc246a5c1162a9661347125f520c81efd15b9a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-11 14:42:31 -0300

    Update desafio040 - Media do aluno.py

--- Mundo 2/desafios/desafio040 - Media do aluno.py
+++ Mundo 2/desafios/desafio040 - Media do aluno.py
@@ -1,8 +1,8 @@
-#Crie um programa que leia duas notas de um aluno e calcule sua media, mostrando uma mensagem
-#no final, de acordo com a media atingida:
-# - media abaixo de 5.0: REPROVADO
-# - media entre 5.0 e 6.9 RECUPERACAO
-# - média 7.0 ou superior APROVADO
+'''Crie um programa que leia duas notas de um aluno e calcule sua media, mostrando uma mensagem
+no final, de acordo com a media atingida:
+ - media abaixo de 5.0: REPROVADO
+ - media entre 5.0 e 6.9 RECUPERACAO
+ - média 7.0 ou superior APROVADO'''
 
 #minha resposta
 #nt1 = float(input('Primeira nota: '))
