09b4be1b44dd895e673e97ea1678fc52d1906c30
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-12 07:47:12 -0300

    Update desafio004_dissecando a variavel.py

--- Mundo 1/desafios/desafio004_dissecando a variavel.py
+++ Mundo 1/desafios/desafio004_dissecando a variavel.py
@@ -16,7 +16,7 @@ print(f"Está capitalizada? ", frase.istitle())
 
 
 #Resposta do Gustavo
-'''a = input('Digite algo: ')
+a = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(a))
 print('Só tem espaços? ', a.isspace())
 print('É um numero? ', a.isnumeric())
@@ -24,5 +24,6 @@ print('É alfabético? ', a.isalpha())
 print('É alfanumérico? ', a.isalnum())
 print('Está em maiusculas? ', a.isupper())
 print('Está em minusculas? ', a.islower())
-print('Está capitalizada? ', a.istitle())'''
+print('Está capitalizada? ', a.istitle())
+
 
