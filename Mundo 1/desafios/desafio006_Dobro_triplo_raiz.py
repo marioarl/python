e6604b23b00c4718e2fd97d97df5d67ce5f3c846
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-12 07:48:12 -0300

    Update desafio006_Dobro_triplo_raiz.py

--- Mundo 1/desafios/desafio006_Dobro_triplo_raiz.py
+++ Mundo 1/desafios/desafio006_Dobro_triplo_raiz.py
@@ -16,6 +16,6 @@ print('A raiz quadrada de {} vale: {}{:.2f}{}'.format(n, cores['azulpreto'], n**
 
 
 #Resposta do Gustavo
-#n = int(input('Digite um numero: '))
-#print('O dobro de {} vale {}'.format(n, (n*2)))
-#print('O triplo de {} vale {}\nA raiz quadrada de {} é igual a {:.2f}'.format(n, (n*3), n, pow(n, (1/2))))
+n = int(input('Digite um numero: '))
+print('O dobro de {} vale {}'.format(n, (n*2)))
+print('O triplo de {} vale {}\nA raiz quadrada de {} é igual a {:.2f}'.format(n, (n*3), n, pow(n, (1/2))))
