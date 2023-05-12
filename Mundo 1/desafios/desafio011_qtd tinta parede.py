c6d4e2f5a19d49ced261347c0fb7178e00401e29
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-12 07:46:09 -0300

    Update desafio011_qtd tinta parede.py

--- Mundo 1/desafios/desafio011_qtd tinta parede.py
+++ Mundo 1/desafios/desafio011_qtd tinta parede.py
@@ -11,10 +11,10 @@ tinta = area / 2
 print('\n\033[31mSua parede tem dimensão de {:.2f} X {:.2f} e sua área é de\033[m \033[7;31m{:.2f}m2\033[m'.format(l, a, area))
 print('\033[33mPara pintar essa parede, voce precisará de\033[m \033[7;33m{:.2f}'.format(tinta),('l de tinta\033[m'))
 
-#resposta do Gustavo
-#larg = float(input('largura da parede: '))
-#alt = float(input('Altura da parede: '))
-#area = larg * alt
-#print('Sua parede tem a dimensão de {}x{} e sua área é de {}m2'.format(larg, alt, area))
-#tinta = area / 2
-#print('Para pintar essa parede, voce precisará de {}l de tinta'.format (tinta))
+#Resposta do Gustavo
+larg = float(input('largura da parede: '))
+alt = float(input('Altura da parede: '))
+area = larg * alt
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m2'.format(larg, alt, area))
+tinta = area / 2
+print('Para pintar essa parede, voce precisará de {}l de tinta'.format (tinta))
