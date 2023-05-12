6a6c4a24d4d35b87dfabf0f67d7a3073e5032cce
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-12 07:48:33 -0300

    Update desafio007_media_aritmetica.py

--- Mundo 1/desafios/desafio007_media_aritmetica.py
+++ Mundo 1/desafios/desafio007_media_aritmetica.py
@@ -7,7 +7,7 @@ n2 = float(input('\n\033[1;33mSegunda nota do aluno:\033[m '))
 print('\033[1;36mA média entre\033[m \033[1;35m{}\033[m e \033[1;33m{}\033[m é igual a \033[1;7;32m{:.2f}\033[m'.format (n1, n2, (n1+n2)/2))
 
 #resposta do Gustavo
-#n1 = float(input('Primeira nota do aluno: '))
-#n2 = float(input('Segunda nota do aluno: '))
-#media = (n1+n2)/2
-#print('A média entre {} e {} é igual a {:.2f}'.format(n1, n2, media))
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+media = (n1+n2)/2
+print('A média entre {} e {} é igual a {:.2f}'.format(n1, n2, media))
