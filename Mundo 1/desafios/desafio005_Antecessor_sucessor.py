2c50f5977b0a028cc19eafb28d25adeda51411c8
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-12 07:47:50 -0300

    Update desafio005_Antecessor_sucessor.py

--- Mundo 1/desafios/desafio005_Antecessor_sucessor.py
+++ Mundo 1/desafios/desafio005_Antecessor_sucessor.py
@@ -8,7 +8,6 @@ a = n - 1
 print('\033[31mO numero é\033[m \033[4;31m{}\033[m \n\033[33mO sucessor é\033[m \033[4;33m{}\033[m'
       ' \n\033[35mO antecessor é\033[m \033[4;35m{}'.format(n, s, a))
 
-
 #resposta do Gustavo
-#n = int(input('Digite um numero: '))
-#print ('Analisando o valor{}, seu antecessor é {} e o sucessor é {}'.format (n, (n-1), (n+1)))
+n = int(input('Digite um numero: '))
+print ('Analisando o valor{}, seu antecessor é {} e o sucessor é {}'.format (n, (n-1), (n+1)))
