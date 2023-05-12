6387cf130938a57c65f13ce61e339e65f20f8e89
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-12 07:43:18 -0300

    Update desafio008_conversor de medida.py

--- Mundo 1/desafios/desafio008_conversor de medida.py
+++ Mundo 1/desafios/desafio008_conversor de medida.py
@@ -6,7 +6,7 @@ metros = float(input('\n\033[1;32mDigite o valor em metros: \033[m'))
 print('\n\033[31mValor em centimetros:\033[m \033[1;30;41m{}cm\033[m\n\033[35mValor em milimetros:\033[m \033[1;45m{}mm\033[m'.format(metros * 100, metros * 1000))
 
 #Resposta do Gustavo
-medida = float(input('Uma distancia em metros: '))
+'''medida = float(input('Uma distancia em metros: '))
 cm = medida * 100
 mm = medida * 1000
-print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(medida, cm, mm))
+print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(medida, cm, mm))'''
