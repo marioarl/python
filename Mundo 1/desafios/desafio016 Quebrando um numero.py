8b63e752df5c1ace54837f692aef5f38fcb63126
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-13 08:02:05 -0300

    Update desafio016 Quebrando um numero.py

--- Mundo 1/desafios/desafio016 Quebrando um numero.py
+++ Mundo 1/desafios/desafio016 Quebrando um numero.py
@@ -5,19 +5,19 @@ inteira EX: Digite o numero: 6.127 O numero 6.127 tem a parte inteira 6
 '''
 #Minha resposta
 from math import trunc
-num = float(input('Digite o numero: '))
-parte = trunc(num)
-print('O valor digitado foi {} e sua porção inteira é {}'.format(num, parte))
+num1 = float(input('Digite o numero: '))
+parte = trunc(num1)
+print('O valor digitado foi {} e sua porção inteira é {}'.format(num1, parte))
 
-#resposta 1 do Gustavo
-#import math
-#num = float(input('Digite um valor: '))
-#print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))
+#Resposta 1 do Gustavo
+import math
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, math.trunc(num)))
 
-#resposta 2 do Gustavo
-#from math import trunc
-#num = float(input('Digite um valor: '))
-#print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
+#Resposta 2 do Gustavo
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
 
 #resposta 3 do Gustavo
 num = float(input('Digite um valor: '))
