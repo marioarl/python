f2d01093925cfb0bc994da98c7f2ff90b6a1bc49
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-13 08:05:10 -0300

    Update desafio020-Sortenado uma ordem.py

--- Mundo 1/desafios/desafio020-Sortenado uma ordem.py
+++ Mundo 1/desafios/desafio020-Sortenado uma ordem.py
@@ -3,7 +3,7 @@ O mesmo professor do desafio anterior quer sortear a ordem de apresentaçao de t
 '''
 
 #minha resposta
-'''import random
+import random
 a1 = str(input('Primeiro Aluno: '))
 a2 = str(input('Segundo Aluno: '))
 a3 = str(input('Terceiro Aluno: '))
@@ -11,7 +11,7 @@ a4 = str(input('Quarto Aluno: '))
 lista = [a1, a2, a3, a4]
 ordem = random.shuffle(lista) #não há necessidade de criar outro variavel(ordem)
 print('A ordem de apresentação será')
-print (lista)'''
+print (lista)
 
 #resposta do Gustavo
 import random
