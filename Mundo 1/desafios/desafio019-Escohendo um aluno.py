192380a78350f1bc0110a8e5e7617ee7ab8dfbc0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-13 08:03:27 -0300

    Update desafio019-Escohendo um aluno.py

--- Mundo 1/desafios/desafio019-Escohendo um aluno.py
+++ Mundo 1/desafios/desafio019-Escohendo um aluno.py
@@ -3,14 +3,14 @@ Um professor quer sortear um dos seus 4 alunos para apagar o quadro. Faça um pr
 ele, lendo o nome deles e escrevendo o nome escolhido
 '''
 
-#minha resposta
-'''import random
+#Minha resposta
+import random
 a1 = str(input ('Primeiro Aluno: '))
 a2 = str(input('Segundo Aluno: '))
 a3 = str(input('Terceiro Aluno: '))
 a4 = str(input('Quarto Aluno: '))
 escolhido = random.choice([a1, a2, a3, a4])
-print('O Aluno escolhido foi', escolhido)'''
+print('O Aluno escolhido foi', escolhido)
 
 #resposat do gustavo
 #import random
