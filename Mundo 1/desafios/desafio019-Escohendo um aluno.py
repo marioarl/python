115eb8e40bcdd06aff816c76839475c237e75529
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-13 08:04:42 -0300

    Update desafio019-Escohendo um aluno.py

--- Mundo 1/desafios/desafio019-Escohendo um aluno.py
+++ Mundo 1/desafios/desafio019-Escohendo um aluno.py
@@ -12,12 +12,12 @@ a4 = str(input('Quarto Aluno: '))
 escolhido = random.choice([a1, a2, a3, a4])
 print('O Aluno escolhido foi', escolhido)
 
-#resposat do gustavo
-#import random
-#n1 = str(input('Primeiro Aluno: '))
-#n2 = str(input('Segundo Aluno: '))
-#n3 = str(input('Terceiro Aluno: '))
-#n4 = str(input('Quarto Aluno: '))
-#lista = [n1, n2, n3, n4]
-#escolhido = random.choice(lista)
-#print('O aluno escolhido foi {}'.format(escolhido))
+#Resposat do gustavo
+import random
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo Aluno: '))
+n3 = str(input('Terceiro Aluno: '))
+n4 = str(input('Quarto Aluno: '))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
