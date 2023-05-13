5f04dcd366198109e551fb489144d5cdacf43b31
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-13 07:58:19 -0300

    Update desafio013_reajuste salarial.py

--- Mundo 1/desafios/desafio013_reajuste salarial.py
+++ Mundo 1/desafios/desafio013_reajuste salarial.py
@@ -3,10 +3,10 @@ Faça um algoritimo que leia o salario de um funcionario e mostre seu novo
 salario com 15% de aumento
 '''
 
-#minha resposta
-#salario = float(input('Qual o salário do funcionário? R$'))
-#aumento = salario + (salario * 0.15)
-#print ('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salario, aumento))
+#Minha resposta
+salario1 = float(input('Qual o salário do funcionário? R$'))
+aumento = salario1 + (salario1 * 0.15)
+print ('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salario1, aumento))
 
 #resposta do Gustavo
 salario = float(input('Qual o salário do funcionário? R$'))
