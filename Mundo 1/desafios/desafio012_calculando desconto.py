433b7d11a30cb9290ec7fb31103e07e9de848344
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-13 07:57:31 -0300

    Update desafio012_calculando desconto.py

--- Mundo 1/desafios/desafio012_calculando desconto.py
+++ Mundo 1/desafios/desafio012_calculando desconto.py
@@ -9,7 +9,7 @@ calculo = produto * 0.05 #5% do valor
 desconto = produto - calculo
 print ('\033[33mO produto que custava \033[1;7;33mR${:.2f}\033[m, \033[33mna promoção com desconto de \033[m\033[1;7;33m5%\033[m \033[33mvai custar \033[1;7;33mR${:.2f}\033[m'.format(produto, desconto))
 
-#resposta do Gustavo
-#preco = float(input('Qual o preço do produto? R$'))
-#novo = preco - (preco * 5 / 100)
-#print ('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preco, novo))
+#Resposta do Gustavo
+preco = float(input('Qual o preço do produto? R$'))
+novo = preco - (preco * 5 / 100)
+print ('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preco, novo))
