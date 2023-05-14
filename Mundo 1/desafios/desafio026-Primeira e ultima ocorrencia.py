cd5a965ae33b52e74d7d9e8a59c1dd975d86e7f6
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-14 08:49:45 -0300

    Update desafio026-Primeira e ultima ocorrencia.py

--- Mundo 1/desafios/desafio026-Primeira e ultima ocorrencia.py
+++ Mundo 1/desafios/desafio026-Primeira e ultima ocorrencia.py
@@ -5,12 +5,12 @@ Faça um programa que leia uma frase pelo teclado e mostre:
  * Em que posição ela aparece a ultima vez
  '''
 
-#minha resposta
-#frase = str(input('Digite uma frase: ')).strip()
-#mfrase = frase.lower()
-#print('A letra A aparece {} vezes na frase'.format(mfrase.count('a')))
-#print('A primeira letra A apareceu na posição {}'.format(mfrase.find('a')))
-#print('A ultima letra A apareceu na posição {}'.format(mfrase.find('a'))) #nao consegui fazer a posicao final
+#Minha resposta
+frase = str(input('Digite uma frase: ')).strip()
+mfrase = frase.lower()
+print('A letra A aparece {} vezes na frase'.format(mfrase.count('a')))
+print('A primeira letra A apareceu na posição {}'.format(mfrase.find('a')+1))
+print('A ultima letra A apareceu na posição {}'.format(mfrase.rfind('a')+1)) 
 
 #resposta do Gustavo
 frase = str(input('Digite uma frase: ')).upper().strip()
