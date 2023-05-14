7beb8c49e7254cf65e982f9e4f695f54a10b4571
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-14 08:52:45 -0300

    Update desafio030-Par ou impar.py

--- Mundo 1/desafios/desafio030-Par ou impar.py
+++ Mundo 1/desafios/desafio030-Par ou impar.py
@@ -1,11 +1,11 @@
 #Crie um programa que leia um numero inteiro e mostre na tela se ele é PAR ou IMPAR
 
-#minha resposta
-#numero = int(input('Me diga um número qualquer: '))
-#if numero % 2 == 0:
-#    print('O numero {} é PAR'.format(numero))
-#else:
-#    print('O numero {} é IMPAR'.format(numero))
+#Minha resposta
+numero = int(input('Me diga um número qualquer: '))
+if numero % 2 == 0:
+    print('O numero {} é PAR'.format(numero))
+else:
+    print('O numero {} é IMPAR'.format(numero))
 
 #resposta do Gustavo
 numero = int(input('Me diga um numero qualquer: '))
