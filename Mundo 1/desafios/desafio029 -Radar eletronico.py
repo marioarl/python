4504688555d3894c1e940a726a049beec2dc5b0d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-14 08:51:51 -0300

    Update desafio029 -Radar eletronico.py

--- Mundo 1/desafios/desafio029 -Radar eletronico.py
+++ Mundo 1/desafios/desafio029 -Radar eletronico.py
@@ -3,15 +3,15 @@ Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h
 uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite
 '''
 
-#minha resposta
-#velo = float(input('Qual é a velocidade do carro? '))
-#multa = (velo - 80) * 7.0
-#if velo <= 80:
-#    print('Tenha um bom dia! Dirija com segurança!')
-#else:
-#    print('MULTADO! Voce excedeu o limite que é de 80Km/h')
-#    print('Voce deve pagar uma multa de R${:.2f}!'.format(multa))
-#    print('Tenha um bom dia! Dirija com segurança!')
+#Minha resposta
+velo = float(input('Qual é a velocidade do carro? '))
+multa = (velo - 80) * 7.0
+if velo <= 80:
+    print('Tenha um bom dia! Dirija com segurança!')
+else:
+    print('MULTADO! Voce excedeu o limite que é de 80Km/h')
+    print('Voce deve pagar uma multa de R${:.2f}!'.format(multa))
+    print('Tenha um bom dia! Dirija com segurança!')
 
 #resposta do Gustavo
 velocidade = float(input('Qual a velocidade atual do carro? '))
