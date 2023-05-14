d9a9e54a3ba0fbec190609116883633ad86a485f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-14 08:56:30 -0300

    Update desafio033-Maior e menor.py

--- Mundo 1/desafios/desafio033-Maior e menor.py
+++ Mundo 1/desafios/desafio033-Maior e menor.py
@@ -1,23 +1,23 @@
 #Faça um programa que leia tres numeros e mostre qual é o maior e qual o menor
 
-#minha resposta
-#n1 = int(input('Primeiro valor: '))
-#n2 = int(input('Segundo valor: '))
-#n3 = int(input('Terceiro valor: '))
-#if n1 < n2 and n1 < n3:
-#    menor = n1
-#if n2 < n1 and n2 < n3:
-#    menor = n2
-#if n3 < n1 and n3 < n2:
-#    menor = n3
-#print('O menor valor digitado foi {}'.format(menor))
-#if n1 > n2 and n1 > n3:
-#    maior = n1
-#if n2 > n1 and n2 > n3:
-#    maior = n2
-#if n3 > n1 and n3 > n2:
-#    maior = n3
-#print('O maior valor digitado foi {}'.format(maior))
+#Minha resposta
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+n3 = int(input('Terceiro valor: '))
+if n1 < n2 and n1 < n3:
+    menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+print('O menor valor digitado foi {}'.format(menor))
+if n1 > n2 and n1 > n3:
+    maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O maior valor digitado foi {}'.format(maior))
 
 #resposta do Gustavo
 a = int(input('Primeiro valor: '))
