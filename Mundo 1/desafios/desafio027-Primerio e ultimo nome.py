5e4b565d91c0f850dc9a629c952f8c18e5758e05
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-14 08:50:23 -0300

    Update desafio027-Primerio e ultimo nome.py

--- Mundo 1/desafios/desafio027-Primerio e ultimo nome.py
+++ Mundo 1/desafios/desafio027-Primerio e ultimo nome.py
@@ -4,12 +4,12 @@ Ex. Ana Maria de Souza
 primeiro = Ana     segundo = Souza
 '''
 
-#minha resposta
-#nome = str(input('Digite seu nome completo: ')).strip().split()
-#ultimo = (len(nome) - 1)
-#print('Muito prazer em te conhecer!')
-#print('Seu primeiro nome é {}'.format(nome[0]))
-#print('Seu ultimo nome é {}'.format(nome[ultimo]))
+#Minha resposta
+nome = str(input('Digite seu nome completo: ')).strip().split()
+ultimo = (len(nome) - 1)
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu ultimo nome é {}'.format(nome[ultimo]))
 
 #resposta do Gustavo
 n = str(input('Digite seu nome completo: ')).strip()
