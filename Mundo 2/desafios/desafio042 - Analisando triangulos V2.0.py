d4fb0d3d5731efbe0d3a66cb582d84d151283a40
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-15 07:56:08 -0300

    Update desafio042 - Analisando triangulos V2.0.py

--- Mundo 2/desafios/desafio042 - Analisando triangulos V2.0.py
+++ Mundo 2/desafios/desafio042 - Analisando triangulos V2.0.py
@@ -5,20 +5,20 @@ isoceles: dois lados iguais
 Escaleno: todos os lados diferentes
 '''
 
-#minha resposta
-#r1 = float(input('Primeiro segmento: '))
-#r2 = float(input('Segundo segmento: '))
-#r3 = float(input('Terceiro segmento: '))
-#if r1 + r2 > r3 and r2 + r3 > r1 and r1 + r3 > r2:
-#    if r1 == r2 == r3:
-#        tipo = 'EQUILATERO'
-#    elif r1 == r2 or r2 == r3 or r1 == r3:
-#        tipo = 'ISOCELES'
-#    elif r1 !=r2 or r2 != r3 or r1 != r3:
-#        tipo = 'ESCALENO'
-#    print('Os segmentos acima PODEM FORMAR um triangulo {}'.format(tipo))
-#else:
-#    print('Os segmentos acima NÃO PODEM FORMAR um triangulo')
+#Minha resposta
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 + r2 > r3 and r2 + r3 > r1 and r1 + r3 > r2:
+    if r1 == r2 == r3:
+        tipo = 'EQUILATERO'
+    elif r1 == r2 or r2 == r3 or r1 == r3:
+        tipo = 'ISOCELES'
+    elif r1 !=r2 or r2 != r3 or r1 != r3:
+        tipo = 'ESCALENO'
+    print('Os segmentos acima PODEM FORMAR um triangulo {}'.format(tipo))
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR um triangulo')
 
 #resposta Gustavo
 r1 = float(input('Primeiro segmento: '))
