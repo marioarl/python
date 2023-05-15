8b94ca325d310fe186390bece441e94bb64eb83c
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-15 07:53:53 -0300

    Update desafio039 - Alistamento militar.py

--- Mundo 2/desafios/desafio039 - Alistamento militar.py
+++ Mundo 2/desafios/desafio039 - Alistamento militar.py
@@ -6,21 +6,21 @@ Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo 
 Seu programa tambem deverá mostrar o tempo que falta ou que passou o prazo
 '''
 
-#minha resposta
-#from datetime import date
-#nasc = int(input('Ano de nascimento: '))
-#data = date.today().year
-#idade = data - nasc
-#anos = 18 - idade
-#print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, data))
-#if idade < 18:
-#    print('Ainda faltam {} anos para o alistamento'.format(anos))
-#    print('Seu alisatamento será em {}'.format(data + anos))
-#elif idade > 18:
-#    print('Voce deveria ter se alistado há {} anos'.format(idade - 18))
-#    print('Seu alistamento foi em {}'.format(nasc + 18))
-#elif idade == 18:
-#    (print('Voce tem que se alistar IMEDIATAMENTE!!!'))
+#Minha resposta
+from datetime import date
+nasc = int(input('Ano de nascimento: '))
+data = date.today().year
+idade = data - nasc
+anos = 18 - idade
+print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, data))
+if idade < 18:
+    print('Ainda faltam {} anos para o alistamento'.format(anos))
+    print('Seu alisatamento será em {}'.format(data + anos))
+elif idade > 18:
+    print('Voce deveria ter se alistado há {} anos'.format(idade - 18))
+    print('Seu alistamento foi em {}'.format(nasc + 18))
+elif idade == 18:
+    (print('Voce tem que se alistar IMEDIATAMENTE!!!'))
 
 #resposta do Gustavo
 from datetime import date
