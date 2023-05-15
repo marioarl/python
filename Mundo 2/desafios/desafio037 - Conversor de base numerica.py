21d340200276a5124c734e10204e91e5a16a6619
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-15 07:52:23 -0300

    Update desafio037 - Conversor de base numerica.py

--- Mundo 2/desafios/desafio037 - Conversor de base numerica.py
+++ Mundo 2/desafios/desafio037 - Conversor de base numerica.py
@@ -4,20 +4,20 @@ a base de conversao:
 1 binario     2 octal     3 hexadecimal
 '''
 
-#minha resposta
-#num = int(input('Digite um número inteiro:'))
-#print('Escolha uma das bases para conversão:')
-#print('[ 1 ] converter para BINÁRIO\n[ 2 ] converter para OCTAL\n[ 3 ] converter para HEXADECIMAL')
-#bin = bin(num)
-#oc = oct(num)
-#hexa = hex(num)
-#opcao = int(input('Escolha sua opcao:'))
-#if opcao == 1:
-#    print('{} convertido para BINARIO é igual a {} '.format(num, bin[2:]))
-#elif opcao == 2:
-#    print('{} convertido para OCTAL é igual a {} '.format(num, oc[2:]))
-#elif opcao == 3:
-#    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hexa[2:]))
+#Minha resposta
+num = int(input('Digite um número inteiro:'))
+print('Escolha uma das bases para conversão:')
+print('[ 1 ] converter para BINÁRIO\n[ 2 ] converter para OCTAL\n[ 3 ] converter para HEXADECIMAL')
+bin = bin(num)
+oc = oct(num)
+hexa = hex(num)
+opcao = int(input('Escolha sua opcao:'))
+if opcao == 1:
+    print('{} convertido para BINARIO é igual a {} '.format(num, bin[2:]))
+elif opcao == 2:
+    print('{} convertido para OCTAL é igual a {} '.format(num, oc[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hexa[2:]))
 
 #resposta do gustavo
 num = int(input('Digite um número inteiro:'))
