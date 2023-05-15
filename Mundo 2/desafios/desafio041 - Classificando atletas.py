d1f754252d65bab31d0b4d5aec89af3ccb218c54
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-15 07:55:32 -0300

    Update desafio041 - Classificando atletas.py

--- Mundo 2/desafios/desafio041 - Classificando atletas.py
+++ Mundo 2/desafios/desafio041 - Classificando atletas.py
@@ -7,22 +7,22 @@ Até 25 anos: SENIOR
 Acima : MASTER
 '''
 
-#minha resposta
-#from datetime import date
-#ano = int(input('Ano de nascimento: '))
-#atual = date.today().year
-#idade = atual - ano
-#print('O atleta tem {} anos'.format(idade))
-#if idade <= 9:
-#    print('Classificação: MIRIM')
-#elif idade > 9 and idade <= 14:
-#    print('Classificação: INFANTIL')
-#elif idade > 14 and idade <= 19:
-#    print('Classificacao: JUNIOR')
-#elif idade >19 and idade <= 25:
-#    print('Classificação: SENIOR')
-#elif idade > 25:
-#    print('Classificação: MASTER')
+#Minha resposta
+from datetime import date
+ano = int(input('Ano de nascimento: '))
+atual = date.today().year
+idade = atual - ano
+print('O atleta tem {} anos'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade > 9 and idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade > 14 and idade <= 19:
+    print('Classificacao: JUNIOR')
+elif idade >19 and idade <= 25:
+    print('Classificação: SENIOR')
+elif idade > 25:
+    print('Classificação: MASTER')
 
 #resposta do Gustavo
 from datetime import date
