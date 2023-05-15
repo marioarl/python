6f2afb57cb0bb0f16de0c2858cbc0cec078451b9
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-15 07:44:22 -0300

    Update desafio034-Aumentos multiplos.py

--- Mundo 1/desafios/desafio034-Aumentos multiplos.py
+++ Mundo 1/desafios/desafio034-Aumentos multiplos.py
@@ -4,13 +4,13 @@ para salarios superiores a R$ 1250,00, calcule um aumento de 10%, para os inferi
 o aumento é de 15%
 '''
 
-#minha resposta
-#salario = float(input('Qual é o salário do funcionario? R$ '))
-#if salario > 1250:
-#    aumento = salario +(salario * 0.1)
-#else:
-#    aumento = salario + (salario * 0.15)
-#print ('Quem ganhava {:.2f} passa a ganhar R${:.2f} agora'.format(salario, aumento))
+#Minha resposta
+salario = float(input('Qual é o salário do funcionario? R$ '))
+if salario > 1250:
+    aumento = salario +(salario * 0.1)
+else:
+    aumento = salario + (salario * 0.15)
+print ('Quem ganhava {:.2f} passa a ganhar R${:.2f} agora'.format(salario, aumento))
 
 #resposta do Gustavo
 salario = float(input('Qual é o salário do funcionario? R$ '))
