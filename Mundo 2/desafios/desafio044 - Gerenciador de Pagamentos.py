c22c90e22001d7319457f1b4bae3931fd9742b54
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-16 07:48:19 -0300

    Update desafio044 - Gerenciador de Pagamentos.py

--- Mundo 2/desafios/desafio044 - Gerenciador de Pagamentos.py
+++ Mundo 2/desafios/desafio044 - Gerenciador de Pagamentos.py
@@ -7,24 +7,25 @@ preço normal e condicao de pagamento:
  - 3x ou mais no cartao: 20% de juros
  '''
 
-#print('='*10, 'LOJAS GUANABARA', '='*10)
-#compra = float(input('Preço das compras: R$ '))
-#print ('FORMAS DE PAGAMENTO\n[ 1 ] à vista dinheiro/cheque\n[ 2 ] à vista cartão\n[ 3 ] 2x no cartão'
-#       '\n[ 4 ] 3x ou mais no cartão')
-#opcao = int(input('Qual é a opção? '))
-#if opcao == 1:
-#    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(compra, compra - (compra * 10 / 100)))
-#elif opcao == 2:
-#    print('Sua compra de R${:.2f} vai custar R${:.2f} no final '.format(compra, compra - (compra * 5 / 100)))
-#elif opcao == 3:
-#    print('Sua compra será parcelada em 2X de R${:.2f} SEM JUROS'.format(compra / 2))
-#if opcao == 4:
-#    parcela = int(input('Quantas parcelas? '))
-#    ncompra = compra + (compra * 20 / 100)
-#    print('Sua compra será parcelada em \033[7;33m{}X\033[m de \033[7;33mR${:.2f}\033[m \033[1;31mCOM JUROS\033[m'.format(parcela, ncompra / parcela ))
-#    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'.format(compra, ncompra))
-#else:
-#    print('\033[7;31mOPCAO INVÁLIDA.\033[m Tente novamente')
+#Minha resposta
+print('='*10, 'LOJAS GUANABARA', '='*10)
+compra = float(input('Preço das compras: R$ '))
+print ('FORMAS DE PAGAMENTO\n[ 1 ] à vista dinheiro/cheque\n[ 2 ] à vista cartão\n[ 3 ] 2x no cartão'
+       '\n[ 4 ] 3x ou mais no cartão')
+opcao = int(input('Qual é a opção? '))
+if opcao == 1:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(compra, compra - (compra * 10 / 100)))
+elif opcao == 2:
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final '.format(compra, compra - (compra * 5 / 100)))
+elif opcao == 3:
+    print('Sua compra será parcelada em 2X de R${:.2f} SEM JUROS'.format(compra / 2))
+if opcao == 4:
+    parcela = int(input('Quantas parcelas? '))
+    ncompra = compra + (compra * 20 / 100)
+    print('Sua compra será parcelada em \033[7;33m{}X\033[m de \033[7;33mR${:.2f}\033[m \033[1;31mCOM JUROS\033[m'.format(parcela, ncompra / parcela ))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final'.format(compra, ncompra))
+else:
+    print('\033[7;31mOPCAO INVÁLIDA.\033[m Tente novamente')
 
 #resposta Gustavo
 print('{:=^40}'.format('LOJAS GUANABARA'))
