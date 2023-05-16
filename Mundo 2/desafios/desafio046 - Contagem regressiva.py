2e822d9eed43d367e892ba6c657d27058878aeb2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-16 07:50:39 -0300

    Update desafio046 - Contagem regressiva.py

--- Mundo 2/desafios/desafio046 - Contagem regressiva.py
+++ Mundo 2/desafios/desafio046 - Contagem regressiva.py
@@ -1,5 +1,7 @@
-#Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de
-#artificio, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+'''
+Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de
+artificio, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+'''
 
 import time
 for c in range(10, -1, -1):
