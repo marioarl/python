28acd3cb849c7886c4aae00dc03a7054f2dcac03
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-17 08:03:36 -0300

    Update desafio050 - Soma dos pares.py

--- Mundo 2/desafios/desafio050 - Soma dos pares.py
+++ Mundo 2/desafios/desafio050 - Soma dos pares.py
@@ -12,3 +12,13 @@ for c in range(1, 7):
         soma = soma + num
         cont = cont + 1
 print('Voce informou {} numeros PARES e a soma foi {}'.format(cont, soma))
+
+#Resposta do Gustavo
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite o {} valor:'.format(c)))
+    if num % 2 == 0:
+        soma = soma + num
+        cont = cont + 1
+print('Voce informou {} numeros PARES e a soma foi {}'.format(cont, soma))
