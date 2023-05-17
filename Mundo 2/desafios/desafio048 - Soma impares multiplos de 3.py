1e25d0792b9d6b1ffa8a3d6c7098aa4ec1ef3058
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-17 07:58:24 -0300

    Update desafio048 - Soma impares multiplos de 3.py

--- Mundo 2/desafios/desafio048 - Soma impares multiplos de 3.py
+++ Mundo 2/desafios/desafio048 - Soma impares multiplos de 3.py
@@ -9,14 +9,14 @@ for i in range(1,500,2):
         soma += i
 print(f'A soma de todos os {cont} valores solicitados é {soma}')
 
-#respotsa Gustavo
-#soma = 0 # acumulador
-#cont = 0 # acumulador
-#for c in range(1, 501, 2):
-#    if c % 3 == 0:
-#        soma = soma + c # pode-se usar a forma simplificada soma += c
-#        cont = cont + 1 # pode-se usar a forma simplificada cont += 1
-#print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
+#Respotsa Gustavo
+soma = 0 # acumulador
+cont = 0 # acumulador
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma = soma + c # pode-se usar a forma simplificada soma += c
+        cont = cont + 1 # pode-se usar a forma simplificada cont += 1
+print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
 
 
 
