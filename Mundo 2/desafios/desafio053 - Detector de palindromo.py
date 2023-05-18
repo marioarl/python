c29a3530cc1d01f29c89478cc27960f22486464f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-18 08:11:39 -0300

    Update desafio053 - Detector de palindromo.py

--- Mundo 2/desafios/desafio053 - Detector de palindromo.py
+++ Mundo 2/desafios/desafio053 - Detector de palindromo.py
@@ -1,6 +1,8 @@
-'''crie um programa que leia uma frase qualquer e diga se ela é um palindrono, desconsiderando
+'''
+Crie um programa que leia uma frase qualquer e diga se ela é um palindrono, desconsiderando
 os espaçoes  EX: APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO
-ANOTARAM A DATA DA MARATONA'''
+ANOTARAM A DATA DA MARATONA
+'''
 
 #minha resposta
 #frase = str(input('Digite uma frase: ')).strip().upper()
