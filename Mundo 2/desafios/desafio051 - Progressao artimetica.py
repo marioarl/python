cf2caf3cbdbece6d3c4e4e9272441af377d803b2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-18 08:03:19 -0300

    Update desafio051 - Progressao artimetica.py

--- Mundo 2/desafios/desafio051 - Progressao artimetica.py
+++ Mundo 2/desafios/desafio051 - Progressao artimetica.py
@@ -1,5 +1,5 @@
-#Crie um programa que leia o primeiro termo e a razao de uma PA. No final, mostre os 10
-#primeiros termos dessa progressao.
+'''Crie um programa que leia o primeiro termo e a razao de uma PA. No final, mostre os 10
+primeiros termos dessa progressao.'''
 
 #minha resposta
 #print('='*30)
