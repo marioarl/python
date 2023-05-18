9fe0656083cd30f004c8d259d3e924ccd2801e55
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-18 08:12:12 -0300

    Update desafio053 - Detector de palindromo.py

--- Mundo 2/desafios/desafio053 - Detector de palindromo.py
+++ Mundo 2/desafios/desafio053 - Detector de palindromo.py
@@ -4,18 +4,18 @@ os espaçoes  EX: APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA 
 ANOTARAM A DATA DA MARATONA
 '''
 
-#minha resposta
-#frase = str(input('Digite uma frase: ')).strip().upper()
-#div = frase.split()
-#all = ''.join(div)
-#inverso = ''
-#for c in range(len(all) - 1, -1, -1):
-#    inverso += all[c]
-#print('O invesro de {} é {}'.format(all, inverso))
-#if inverso == all:
-#    print('Temos um palindomo!')
-#else:
-#    print('A frase digitada não é um palindromo')
+#Minha resposta
+frase = str(input('Digite uma frase: ')).strip().upper()
+div = frase.split()
+all = ''.join(div)
+inverso = ''
+for c in range(len(all) - 1, -1, -1):
+    inverso += all[c]
+print('O invesro de {} é {}'.format(all, inverso))
+if inverso == all:
+    print('Temos um palindomo!')
+else:
+    print('A frase digitada não é um palindromo')
 
 #resposta1 do Gustavo
 #frase = str(input('Digite uma frase: ')).strip().upper()
