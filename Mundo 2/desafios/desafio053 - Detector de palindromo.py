a9f201b5cb845cc6bb725d5b56b4e0e1e72207b4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-18 08:13:03 -0300

    Update desafio053 - Detector de palindromo.py

--- Mundo 2/desafios/desafio053 - Detector de palindromo.py
+++ Mundo 2/desafios/desafio053 - Detector de palindromo.py
@@ -17,18 +17,18 @@ if inverso == all:
 else:
     print('A frase digitada não é um palindromo')
 
-#resposta1 do Gustavo
-#frase = str(input('Digite uma frase: ')).strip().upper()
-#div = frase.split()
-#all = ''.join(div)
-#inverso = ''
-#for c in range(len(all) - 1, -1, -1):
-#    inverso += all[c]
-#print('O invesro de {} é {}'.format(all, inverso))
-#if inverso == all:
-#    print('Temos um palindomo!')
-#else:
-#    print('A frase digitada não é um palindromo')
+#Resposta1 do Gustavo
+frase = str(input('Digite uma frase: ')).strip().upper()
+div = frase.split()
+all = ''.join(div)
+inverso = ''
+for c in range(len(all) - 1, -1, -1):
+    inverso += all[c]
+print('O invesro de {} é {}'.format(all, inverso))
+if inverso == all:
+    print('Temos um palindomo!')
+else:
+    print('A frase digitada não é um palindromo')
 
 #resposta2 do Gustavo
 frase = str(input('Digite uma frase: ')).strip().upper()
