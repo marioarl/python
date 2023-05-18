9bfe2db416b094b37e00af39ff45e716f7eec701
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-18 08:10:55 -0300

    Update desafio052 - Numeros Primos.py

--- Mundo 2/desafios/desafio052 - Numeros Primos.py
+++ Mundo 2/desafios/desafio052 - Numeros Primos.py
@@ -15,3 +15,19 @@ if tot == 2:
     print('E por isso ele É PRIMO!')
 else:
     print('E por isso ele NÃO É PRIMO!')
+
+#Resposta do Gustavo
+num = int(input('Digite um numero: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mO numero {} foi divisivel {} vezes'.format(num, tot))
+if tot == 2:
+    print('E por isso ele É PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
