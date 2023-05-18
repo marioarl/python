14c6508e7c25b205c2ff4c46248cf90f9652f797
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-18 08:04:34 -0300

    Update desafio051 - Progressao artimetica.py

--- Mundo 2/desafios/desafio051 - Progressao artimetica.py
+++ Mundo 2/desafios/desafio051 - Progressao artimetica.py
@@ -3,16 +3,16 @@ Crie um programa que leia o primeiro termo e a razao de uma PA. No final, mostre
 primeiros termos dessa progressao.
 '''
 
-#minha resposta
-#print('='*30)
-#print('{:^30}'.format('10 TERMOS DE UMA PA'))
-#print('='*30)
-#termo = int(input('Primeiro termo: '))
-#razao = int(input('Razão: '))
-#an = termo + (10 -1) * razao #formula da PA
-#for m in range(termo, an + 1 , razao): # an + 1 porque o Python pega o n + 1
-#    print('{} ->'.format(m), end=' ')
-#print('ACABOU')
+#Minha resposta
+print('='*30)
+print('{:^30}'.format('10 TERMOS DE UMA PA'))
+print('='*30)
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+an = termo + (10 -1) * razao #formula da PA
+for m in range(termo, an + 1 , razao):
+    print('{} ->'.format(m), end=' ')
+print('ACABOU')
 
 #resposta gustavo
 primeiro = int(input('Primeiro termo: '))
