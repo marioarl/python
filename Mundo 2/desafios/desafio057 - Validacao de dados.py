efa12f707cd0c80280f0ba97db91f971531725b1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-19 07:59:11 -0300

    Update desafio057 - Validacao de dados.py

--- Mundo 2/desafios/desafio057 - Validacao de dados.py
+++ Mundo 2/desafios/desafio057 - Validacao de dados.py
@@ -3,13 +3,13 @@ Crie um programa que leia o sexo de uma pessoa, mas só aceite os valores M ou F
 Caso esteja errado, peça a digitacao novamente até ter um valor correto
 '''
 
-#minha resposta
-#sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()
-#while sexo != 'M' and sexo != 'F':
-#    sexo = str(input('Dados inválidos. Por favor, informe seu sexo:')).strip().upper()
-#print('Sexo {} registrado com sucesso'.format(sexo))
+#Minha resposta
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo:')).strip().upper()
+print('Sexo {} registrado com sucesso'.format(sexo))
 
-#resposta do Gustavo
+#Resposta do Gustavo
 sexo = str(input('Informe seu sexo: [M,F]')).strip().upper()[0] #o [0] significa se escrever Masculino ou Feminino, pegará somente a primeira letra
 while sexo not in 'MmFf':
     sexo = str(input('Dados invalidos. Por favor, informe seu sexo: ')).strip().upper()[0]
