6605467fb0a619331983aa2067c87e9991e0001f
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-20 09:44:36 -0300

    Update desafio060 - Calculo do Fatorial.py

--- Mundo 2/desafios/desafio060 - Calculo do Fatorial.py
+++ Mundo 2/desafios/desafio060 - Calculo do Fatorial.py
@@ -2,6 +2,7 @@
 Faça um programa que leia um numero qualquqer e mostre seu fatorial
 EX: 5!=5X4X3X2X1 = 120
 '''
+#Minha Resposta
 print('Digite um numero para')
 num = int(input('calcular seu fatorial: '))
 fat2 = 0
@@ -9,11 +10,11 @@ for c in range(1, num, -1):
     fat = num * c
     print('Calculando {}! = {} X {}'.format(num, num, c))
 
-#resposta1 Gustavo(com modulo math
-#from math import factorial
-#n = int(input('Digite um número para calcular seu fatorail: '))
-#f = factorial(n)
-#print('O fatorial de {} é {}'.format(n, f))
+#Resposta1 Gustavo(com modulo math
+from math import factorial
+n = int(input('Digite um número para calcular seu fatorail: '))
+f = factorial(n)
+print('O fatorial de {} é {}'.format(n, f))
 
 #resposta2 Gustavo
 n = int(input('Digite um numero para calcular seu fatorial: '))
