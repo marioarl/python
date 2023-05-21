f93440e39b2cbe2f6ff7d2b9df818ebdc571de3a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-21 09:08:32 -0300

    Update desafio063 - Sequencia Fibonacci.py

--- Mundo 2/desafios/desafio063 - Sequencia Fibonacci.py
+++ Mundo 2/desafios/desafio063 - Sequencia Fibonacci.py
@@ -20,3 +20,22 @@ while cont <= termos:
     t2 = t3 #trnasicao de termos
     cont += 1
 print(' -> FIM')
+
+#Resposta do Gustavo
+print('-'*30)
+print('Sequencia Fibonacci')
+print('-'*30)
+n = int(input('Quantos termos voce quer mostrar? '))
+t1 = 0
+t2 = 1
+print('~'*30)
+print('{} -> {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print('-> {}'.format(t3), end='')
+    t1 = t2 
+    t2 = t3 
+    cont += 1
+print(' -> FIM')
+print('~'*30)
