b08bcf2a6d9b2b12317990a1bcb2dad5abd4c9e9
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-21 09:03:34 -0300

    Update desafio062 - Progressao aritimetica v3.py

--- Mundo 2/desafios/desafio062 - Progressao aritimetica v3.py
+++ Mundo 2/desafios/desafio062 - Progressao aritimetica v3.py
@@ -20,3 +20,23 @@ while mais != 0:
     print('PAUSA')
     mais = int(input('Quantos termos voce quer mostrar a mais? '))
 print('Progressao finalizada com {} termos mostrados'.format(total))
+
+#Resposta do Gustavo
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos voce quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
+print('FIM')
