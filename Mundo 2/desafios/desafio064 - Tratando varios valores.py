fc982509315506ade03b48747e685ec8271ce49d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-21 09:09:22 -0300

    Update desafio064 - Tratando varios valores.py

--- Mundo 2/desafios/desafio064 - Tratando varios valores.py
+++ Mundo 2/desafios/desafio064 - Tratando varios valores.py
@@ -1,5 +1,7 @@
-'''Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar quando o usuario digitar o valor 999, que é a condicao de parada. No final mostre quantos numeros foram digitados
-e qual foi a soma entre eles, desconsiderando o 999.'''
+'''
+Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar quando o usuario digitar o valor 999, que é a condicao de parada. No final mostre quantos numeros foram digitados
+e qual foi a soma entre eles, desconsiderando o 999.
+'''
 
 """"
 Minha resposta
