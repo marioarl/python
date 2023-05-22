c91174e9fc27d02f5d34cb776e2a918b415c78f0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-22 08:04:20 -0300

    Update desafio065 - Maior e menor valores.py

--- Mundo 2/desafios/desafio065 - Maior e menor valores.py
+++ Mundo 2/desafios/desafio065 - Maior e menor valores.py
@@ -1,6 +1,6 @@
-#crie um programa que leia varios numeros inteiros pelo teclado. No final da execucao, mostre
-#a media entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao
-#usuario se ele quer ou nao continuar a digitar
+'''Crie um programa que leia varios numeros inteiros pelo teclado. No final da execucao, mostre
+a media entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao
+usuario se ele quer ou nao continuar a digitar'''
 
 #minha resposta
 """"
