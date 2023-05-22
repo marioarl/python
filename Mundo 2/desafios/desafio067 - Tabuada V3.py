3179b831d75b8dce386c44c7263dffa65d8631a3
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-22 08:06:50 -0300

    Update desafio067 - Tabuada V3.py

--- Mundo 2/desafios/desafio067 - Tabuada V3.py
+++ Mundo 2/desafios/desafio067 - Tabuada V3.py
@@ -1,5 +1,7 @@
-'''Faca um programa que mostre a tabuada de varios numeros, um de cada vez, para cada valor
-digitado pelo usuario. O programa sera interrompido quando o numero solicitado for negativo'''
+'''
+Faca um programa que mostre a tabuada de varios numeros, um de cada vez, para cada valor
+digitado pelo usuario. O programa sera interrompido quando o numero solicitado for negativo
+'''
 
 #minha resposta
 while True:
