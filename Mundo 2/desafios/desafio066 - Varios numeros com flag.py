2363d2b50e1ed73c77d1da310e25e8d867047e05
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-22 08:05:41 -0300

    Update desafio066 - Varios numeros com flag.py

--- Mundo 2/desafios/desafio066 - Varios numeros com flag.py
+++ Mundo 2/desafios/desafio066 - Varios numeros com flag.py
@@ -1,6 +1,7 @@
-#Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar
-# quando o usuario digitar o valor 999, que é a condicao de parada . No final mostre quantos
-#numeros foram digitados e qual foi a soma entre eles(desconsiderando o flag)
+'''Crie um programa que leia varios numeros inteiros pelo teclado. O programa só vai parar
+quando o usuario digitar o valor 999, que é a condicao de parada . No final mostre quantos
+numeros foram digitados e qual foi a soma entre eles(desconsiderando o flag)'''
+
 #minha resposta
 cont = soma = 0
 while True:
