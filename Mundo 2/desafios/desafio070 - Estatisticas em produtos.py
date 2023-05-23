47b295258b99a2743eef197af23cab616f20c766
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-23 09:51:12 -0300

    Update desafio070 - Estatisticas em produtos.py

--- Mundo 2/desafios/desafio070 - Estatisticas em produtos.py
+++ Mundo 2/desafios/desafio070 - Estatisticas em produtos.py
@@ -1,8 +1,10 @@
-'''Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se
+'''
+Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se
 o usuario vai continuar. No final mostre:
- A) Qual é o total gasto na compra
- B) Quantos produtos custam mais de R$1000
- C) Qual é o nome do produto mais barato'''
+A) Qual é o total gasto na compra
+B) Quantos produtos custam mais de R$1000
+C) Qual é o nome do produto mais barato
+'''
 
 #minha resposta
 """print('-'*30)
