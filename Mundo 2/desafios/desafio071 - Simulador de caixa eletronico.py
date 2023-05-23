cb24dabbadd9386ad2eddb33e30a12211c7ab73e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-23 09:52:22 -0300

    Update desafio071 - Simulador de caixa eletronico.py

--- Mundo 2/desafios/desafio071 - Simulador de caixa eletronico.py
+++ Mundo 2/desafios/desafio071 - Simulador de caixa eletronico.py
@@ -1,7 +1,7 @@
-#Crie um programa que simule o funcionamento de um caixa eletronico. No inicio, pergunte
-#ao usuario qual será o valor a ser sacado(numero inteiro) e o programa vai informar quantas
-#cedulas de cada valor serao entregues.
-#OBS: Considere que o caixa possui cedulas de R$50 R$20 R$10 e R$1
+'''Crie um programa que simule o funcionamento de um caixa eletronico. No inicio, pergunte
+ao usuario qual será o valor a ser sacado(numero inteiro) e o programa vai informar quantas
+cedulas de cada valor serao entregues.
+OBS: Considere que o caixa possui cedulas de R$50 R$20 R$10 e R$1'''
 
 #minha resposta
 """print("="*40)
