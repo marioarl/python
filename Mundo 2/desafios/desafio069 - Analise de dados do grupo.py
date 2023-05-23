e2bcf859c94a46f4b7847617eb07c621ff726d59
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-23 09:49:13 -0300

    Update desafio069 - Analise de dados do grupo.py

--- Mundo 2/desafios/desafio069 - Analise de dados do grupo.py
+++ Mundo 2/desafios/desafio069 - Analise de dados do grupo.py
@@ -1,8 +1,9 @@
-#crie um programa que leia a idade e o sexo de varias pessoas. A cada pessoa cadastrada, o
-#programa deverá perguntar se o usuario quer ou nao continuar. No final mostre:
-#A) quantas pessoas tem mais de 18 anos
-#B) quantos homens foram cadastrados
-#C) quantas mulheres tem menos de 20 anos.
+'''Crie um programa que leia a idade e o sexo de varias pessoas. A cada pessoa cadastrada, o
+programa deverá perguntar se o usuario quer ou nao continuar. No final mostre:
+A) quantas pessoas tem mais de 18 anos
+B) quantos homens foram cadastrados
+C) quantas mulheres tem menos de 20 anos.'''
+
 #minha resposta
 mais18 = h = m = menos20 = 0
 while True:
