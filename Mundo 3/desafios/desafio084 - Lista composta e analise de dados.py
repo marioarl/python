fba8b5f9e2a45a12c365584710e61c7c94948015
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-25 14:57:16 -0300

    Update desafio084 - Lista composta e analise de dados.py

--- Mundo 3/desafios/desafio084 - Lista composta e analise de dados.py
+++ Mundo 3/desafios/desafio084 - Lista composta e analise de dados.py
@@ -1,7 +1,10 @@
-#crie um programa que leia nome e peso de varias pessoas guardando tudo em uma lista. No final mostre
-#A)Quantas pessoas foram cadastradas.
-#B)Uma listagem com as pessoas mais pesadas
-#C)Uma listagem com as pessoas mais leves
+'''
+Crie um programa que leia nome e peso de varias pessoas guardando tudo em uma lista. No final mostre
+A)Quantas pessoas foram cadastradas.
+B)Uma listagem com as pessoas mais pesadas
+C)Uma listagem com as pessoas mais leves
+'''
+
 dados = list()
 pessoas = list()
 mai = men = 0
