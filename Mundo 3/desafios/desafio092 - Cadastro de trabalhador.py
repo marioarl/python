27689e3854d4acf0d13641dc589bded905c334e9
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-29 13:05:38 -0300

    Update desafio092 - Cadastro de trabalhador.py

--- Mundo 3/desafios/desafio092 - Cadastro de trabalhador.py
+++ Mundo 3/desafios/desafio092 - Cadastro de trabalhador.py
@@ -7,10 +7,26 @@ Calclue e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar.
 #minha resposta
 from datetime import date
 atual = date.today().year
+lista = dict()
+lista['nome'] = str(input('Nome: ').strip())
+ano = int(input('Ano de nascimento: '))
+lista['idade'] = atual - ano
+lista['ctps'] = int(input('Carteira de trabalho (0 não tem): '))
+if lista['ctps'] != 0:
+    lista['contratacao'] = int(input('Ano de contratacao: '))
+    lista['salario'] = float(input('Salario: '))
+    lista['aposentadoria'] = (lista['contratacao'] + 35) - ano
+print('-='*40)
+print(lista)
+for k, i in lista.items():
+    print(f'{k} tem o valor {i}')
+
+#resposta do Gustavo igual
+hoje = date.today().year
 ficha = dict()
 ficha['nome'] = str(input('Nome: ').strip())
 ano = int(input('Ano de nascimento: '))
-ficha['idade'] = atual - ano
+ficha['idade'] = hoje - ano
 ficha['ctps'] = int(input('Carteira de trabalho (0 não tem): '))
 if ficha['ctps'] != 0:
     ficha['contratacao'] = int(input('Ano de contratacao: '))
@@ -20,5 +36,3 @@ print('-='*40)
 print(ficha)
 for k, i in ficha.items():
     print(f'{k} tem o valor {i}')
-
-#resposta do Gustavo igual
