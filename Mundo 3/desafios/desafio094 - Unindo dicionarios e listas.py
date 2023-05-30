1ff6e8a71f9d6460cea2010b1b127d43f9214d52
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-30 09:36:19 -0300

    Update desafio094 - Unindo dicionarios e listas.py

--- Mundo 3/desafios/desafio094 - Unindo dicionarios e listas.py
+++ Mundo 3/desafios/desafio094 - Unindo dicionarios e listas.py
@@ -5,6 +5,47 @@ B)A média de idade do grupo
 C)Uma lista com todas as mulheres.
 D)Uma lista com todas as pessoas com idade acima da média
 '''
+#Minha resposta
+todos = list()
+unico = dict()
+soma = media = 0
+while True:
+    unico.clear()
+    unico['nome'] = str(input('Nome: ').strip())
+    while True:
+        unico['sexo'] = str(input('Sexo: [M/F] ').strip().upper()[0])
+        if unico['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor digite apenas M ou F.')
+    unico['idade'] = int(input('Idade: '))
+    soma += unico['idade']
+    todos.append(unico.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N')
+    if resp == 'N':
+        break
+print(f'A) Ao todo temos {len(todos)} pessoas cadastradas')
+media = soma / len(todos)
+print(f'B) A média de idade é de {media:5.2f} anos.')
+print('C) As mulheres cadastradas foram: ', end='')
+for p in todos:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]}, ', end='')
+print()
+print('D) Lista de pessoas que estão acima da média: ', end='')
+print()
+for p in todos:
+    if p['idade'] >= media:
+        print('     ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<<<ENCERRADO>>>')
+
+
 
 #Resposta do Gustavo
 galera = list()
