1b36e88af1fa2ea1a6e5677d3077ed30449d540a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-05-30 09:30:08 -0300

    Update desafio094 - Unindo dicionarios e listas.py

--- Mundo 3/desafios/desafio094 - Unindo dicionarios e listas.py
+++ Mundo 3/desafios/desafio094 - Unindo dicionarios e listas.py
@@ -1,9 +1,9 @@
-# Crie um prg que leia nome, sexo e idade de varias pessoas, guardando os dados de cada pessoa em um
-# dicionario e todos os dicionarios em uma lista. No final, mostre:
-# A)Quantas pessoas foram cadastradas
-# B)A média de idade do grupo
-# C)Uma lista com todas as mulheres.
-# D)Uma lista com todas as pessoas com idade acima da média
+'''Crie um prg que leia nome, sexo e idade de varias pessoas, guardando os dados de cada pessoa em um
+dicionario e todos os dicionarios em uma lista. No final, mostre:
+A)Quantas pessoas foram cadastradas
+B)A média de idade do grupo
+C)Uma lista com todas as mulheres.
+D)Uma lista com todas as pessoas com idade acima da média'''
 galera = list()
 pessoa = dict()
 soma = media = 0
