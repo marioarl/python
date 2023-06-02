640de69fea603fa1c3a08b745d072d9024470ad6
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-02 08:39:03 -0300

    Update desafio101 - Funcoes para votacao.py

--- Mundo 3/desafios/desafio101 - Funcoes para votacao.py
+++ Mundo 3/desafios/desafio101 - Funcoes para votacao.py
@@ -1,6 +1,8 @@
-'''Crie um programa que tenha uma funcao chamada voto() que vai receber como parametro o ano de nascimento
+'''
+Crie um programa que tenha uma funcao chamada voto() que vai receber como parametro o ano de nascimento
 de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas
-eleicoes.'''
+eleicoes.
+'''
 
 #minha resposta
 def voto(ano):
