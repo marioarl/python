ee7de5a6be8692d794b6547ea8506d3c584d82a1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-03 12:04:07 -0300

    Update desafio109 - Formatando moedas V2.py

--- Mundo 3/desafios/desafio109 - Formatando moedas V2.py
+++ Mundo 3/desafios/desafio109 - Formatando moedas V2.py
@@ -1,4 +1,5 @@
-'''Modifique as funcoes que foram criadas no desafio107 para que elas aceitem um parametro a mais,
+'''
+Modifique as funcoes que foram criadas no desafio107 para que elas aceitem um parametro a mais,
 informando se o valor retornado por elas vai ser ou nao formatado pela funcao moeda(), desenvolvida no
 desafio108'''
 from ex109 import moeda
