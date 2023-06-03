5a8e5fb5c28515de427db7545b47cacd8c409306
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-03 12:03:52 -0300

    Update desafio109 - Formatando moedas V2.py

--- Mundo 3/desafios/desafio109 - Formatando moedas V2.py
+++ Mundo 3/desafios/desafio109 - Formatando moedas V2.py
@@ -1,6 +1,6 @@
-#Modifique as funcoes que foram criadas no desafio107 para que elas aceitem um parametro a mais,
-#informando se o valor retornado por elas vai ser ou nao formatado pela funcao moeda(), desenvolvida no
-#desafio108
+'''Modifique as funcoes que foram criadas no desafio107 para que elas aceitem um parametro a mais,
+informando se o valor retornado por elas vai ser ou nao formatado pela funcao moeda(), desenvolvida no
+desafio108'''
 from ex109 import moeda
 p = float(input('Digite o preço: R$ '))
 print(f'A metade de {moeda.moeda(p)} é {(moeda.metade(p, True))}')
