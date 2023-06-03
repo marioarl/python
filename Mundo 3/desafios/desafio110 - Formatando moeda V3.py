fdfda2142b2fcea5a1bc47e9463d866980444acf
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-03 12:05:05 -0300

    Update desafio110 - Formatando moeda V3.py

--- Mundo 3/desafios/desafio110 - Formatando moeda V3.py
+++ Mundo 3/desafios/desafio110 - Formatando moeda V3.py
@@ -1,5 +1,4 @@
-#Adicione ao modulo moeda.py criado nos desafios anteriores, uma funcao chamada resumo(), que mostra na
-#tela algumas informacoes geradas pelas funcoes que já temos no modulo criado até aqui
+'''Adicione ao modulo moeda.py criado nos desafios anteriores, uma funcao chamada resumo(), que mostra na tela algumas informacoes geradas pelas funcoes que já temos no modulo criado até aqui'''
 from ex110 import moeda
 
 p = float(input('Digite o preço: R$'))
