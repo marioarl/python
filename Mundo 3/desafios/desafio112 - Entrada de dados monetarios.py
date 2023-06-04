1331eb7cd7138623eca7524695e48132c5ea79ab
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-04 10:04:31 -0300

    Update desafio112 - Entrada de dados monetarios.py

--- Mundo 3/desafios/desafio112 - Entrada de dados monetarios.py
+++ Mundo 3/desafios/desafio112 - Entrada de dados monetarios.py
@@ -1,6 +1,6 @@
-#Dentro do pacote utilidadesCev que criamos no desafo111, temos o modulo chamado dado. Crie uma funcao
-# chamada leiaDinheiro() que seja capaz de funcionar com a funcao input(), mas com uma validacao de dados
-#para aceitar apenas valores que sejam monetarios.
+'''Dentro do pacote utilidadesCev que criamos no desafo111, temos o modulo chamado dado. Crie uma funcao
+chamada leiaDinheiro() que seja capaz de funcionar com a funcao input(), mas com uma validacao de dados
+para aceitar apenas valores que sejam monetarios.'''
 from ex112.utilidadescev import moeda
 from ex112.utilidadescev import dado
 
