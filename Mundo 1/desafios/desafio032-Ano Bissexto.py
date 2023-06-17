59f506053a4c00f4412df71657ff76fc2fb26f4d
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-17 12:26:48 -0300

    Update desafio032-Ano Bissexto.py

--- Mundo 1/desafios/desafio032-Ano Bissexto.py
+++ Mundo 1/desafios/desafio032-Ano Bissexto.py
@@ -11,7 +11,7 @@ if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
 else:
     print(f"não é BISSEXTO")
 
-#resposta do Gustavo
+#Resposta do Gustavo
 from datetime import date
 ano = int(input('Que ano quer analisar? Coloque 0 para o ano atual: '))
 if ano == 0:
