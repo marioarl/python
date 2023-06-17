30018a2f7f589f4db35243f692cedc38fef74190
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-17 12:24:46 -0300

    Update desafio017-Cateto e Hipotenusa.py

--- Mundo 1/desafios/desafio017-Cateto e Hipotenusa.py
+++ Mundo 1/desafios/desafio017-Cateto e Hipotenusa.py
@@ -1,6 +1,7 @@
 '''
 Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um
-triangulo retangulo, calcule e mostre o comprimento da hipotenusa'''
+triangulo retangulo, calcule e mostre o comprimento da hipotenusa
+'''
 
 #minha resposta 1
 ''''import math
