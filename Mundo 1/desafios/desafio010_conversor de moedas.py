bc1e1c2dc71f321a23367f974059911729ed77e1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-17 12:24:07 -0300

    Update desafio010_conversor de moedas.py

--- Mundo 1/desafios/desafio010_conversor de moedas.py
+++ Mundo 1/desafios/desafio010_conversor de moedas.py
@@ -1,4 +1,5 @@
-'''Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar. Considere US$ 1,00 = R$ 3,27'''
+'''Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar. Considere US$ 1,00 = R$ 3,27
+'''
 
 #Minha resposta
 print ('='*50)
