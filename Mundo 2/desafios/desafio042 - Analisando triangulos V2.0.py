756f3bb2d79540c7dc7df9b8bbd04249c57b2d05
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-18 18:06:24 -0300

    Update desafio042 - Analisando triangulos V2.0.py

--- Mundo 2/desafios/desafio042 - Analisando triangulos V2.0.py
+++ Mundo 2/desafios/desafio042 - Analisando triangulos V2.0.py
@@ -20,7 +20,7 @@ if r1 + r2 > r3 and r2 + r3 > r1 and r1 + r3 > r2:
 else:
     print('Os segmentos acima NÃO PODEM FORMAR um triangulo')
 
-#resposta Gustavo
+#Resposta Gustavo
 r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
