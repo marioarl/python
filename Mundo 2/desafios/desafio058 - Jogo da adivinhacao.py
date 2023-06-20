5f7ce1d630dab4d0de81978d32ef03458f871965
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-20 15:17:20 -0300

    Update desafio058 - Jogo da adivinhacao.py

--- Mundo 2/desafios/desafio058 - Jogo da adivinhacao.py
+++ Mundo 2/desafios/desafio058 - Jogo da adivinhacao.py
@@ -27,7 +27,7 @@ else:
     print('Acertou em {} tentativas, PARABENS!'.format(tentativas))
 
 
-#resposta do gustavo
+#Resposta do gustavo
 from random import randint
 computador = randint(0, 10)
 print('Sou seu computador....Acabei de pensar em um numero entre 0 e 10')
