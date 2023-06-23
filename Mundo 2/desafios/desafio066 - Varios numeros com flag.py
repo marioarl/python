e9bc71256e99ce76e0b10e7369aad0f5adb20ff7
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-06-23 10:20:24 -0300

    Update desafio066 - Varios numeros com flag.py

--- Mundo 2/desafios/desafio066 - Varios numeros com flag.py
+++ Mundo 2/desafios/desafio066 - Varios numeros com flag.py
@@ -14,7 +14,7 @@ while True:
     cont += 1
 print(f'A soma dos {cont} valores foi {soma}')
 
-#resposta do Gustavo
+#Resposta do Gustavo
 soma = cont = 0
 while True:
     num = int(input('Digite um valor (999 para parar): '))
